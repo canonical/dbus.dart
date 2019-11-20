b57cfbfd5924fab4366d8a69bcd72c32b33da823
Robert Ancell <robert.ancell@canonical.com>
2019-11-21 12:12:22 +1300

    Add a session bus example

---
+++ examples/notifications.dart
@@ -0,0 +1,23 @@
+import 'dbus_client.dart';
+
+main() async {
+  var client = DBusClient.session();
+  await client.connect();
+  var values = [
+    new DBusString(''),                                           // App name
+    new DBusUint32(0),                                            // Replaces
+    new DBusString(''),                                           // Icon
+    new DBusString('Hello World!'),                               // Summary
+    new DBusString(''),                                           // Body
+    new DBusArray(new DBusSignature('s')),                        // Actions
+    new DBusDict(new DBusSignature('s'), new DBusSignature('v')), // Hints
+    new DBusInt32(-1),                                            // Expire timeout
+  ];
+  var result = await client.callMethod(destination: 'org.freedesktop.Notifications',
+                                       path: '/org/freedesktop/Notifications',
+                                       interface: 'org.freedesktop.Notifications',
+                                       member: 'Notify',
+                                       values: values);
+  var id = (result[0] as DBusUint32).value;
+  print('notify ${id}');
+}
