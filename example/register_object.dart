e5d681496feb964b32a6737e36644ca18154e90d
Robert Ancell <robert.ancell@canonical.com>
2020-08-05 16:44:32 +1200

    Register object paths and make introspectable

--- example/register_object.dart
+++ example/register_object.dart
@@ -1,12 +1,13 @@
 import 'package:dbus_client/dbus_client.dart';
 
 // Test with:
-// $ gdbus call --session --dest com.canonical.DBusDart --object-path / --method com.canonical.DBusDart.Test
+// $ gdbus call --session --dest com.canonical.DBusDart --object-path /com/canonical/DBusDart --method com.canonical.DBusDart.Test
 
 main() async {
   var client = DBusClient.session();
   await client.connect();
   await client.requestName('com.canonical.DBusDart');
+  client.registerObject('/com/canonical/DBusDart');
   client.listenMethod('com.canonical.DBusDart', (String path, String interface,
       String member, List<DBusValue> values) async {
     if (member == 'Test') {
