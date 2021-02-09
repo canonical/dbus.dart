9f726939bd5ccf814be1d6ef946fceb5ffdaf067
Robert Ancell <robert.ancell@canonical.com>
2021-02-10 11:30:09 +1300

    Refactor DBusAddress constructor

--- lib/src/dbus_address.dart
+++ lib/src/dbus_address.dart
@@ -19,20 +19,24 @@ class DBusAddress {
   /// Transport properties, e.g. 'path': '/run/user/1000/bus'.
   List<DBusAddressProperty> properties;
 
+  /// Creates a new address using [transport] and [properties]
+  DBusAddress(this.transport, this.properties);
+
   /// Creates a new address from the given [address] string, e.g. 'unix:path=/run/user/1000/bus'.
-  DBusAddress(String address) {
+  factory DBusAddress.fromString(String address) {
     // Addresses are in the form 'transport:key1=value1,key2=value2'
     var index = address.indexOf(':');
     if (index < 0) {
       throw 'Unable to determine transport of D-Bus address: ${address}';
     }
 
-    transport = address.substring(0, index);
-    properties = _parseProperties(address.substring(index + 1));
+    var transport = address.substring(0, index);
+    var properties = _parseProperties(address.substring(index + 1));
+    return DBusAddress(transport, properties);
   }
 
   /// Parse properties from a property list, e.g. 'path=/run/user/1000/bus'.
-  List<DBusAddressProperty> _parseProperties(String propertiesList) {
+  static List<DBusAddressProperty> _parseProperties(String propertiesList) {
     var properties = <DBusAddressProperty>[];
     if (propertiesList == '') {
       return properties;
@@ -57,7 +61,7 @@ class DBusAddress {
   }
 
   /// Decode an escaped value, e.g. 'Hello%20World' -> 'Hello World'.
-  String _decodeValue(String encodedValue) {
+  static String _decodeValue(String encodedValue) {
     var escapedValue = utf8.encode(encodedValue);
     var binaryValue = <int>[];
     for (var i = 0; i < escapedValue.length; i++) {
@@ -82,7 +86,7 @@ class DBusAddress {
   }
 
   /// Decode a hex ASCII code to its decimal value. e.g. 'D' -> 13.
-  int _hexCharToDecimal(int value) {
+  static int _hexCharToDecimal(int value) {
     final zero = 48; // '0'
     final nine = 57; // '9'
     final A = 65; // 'A'
