b675a7fca0f2adb4fd30632ac030ea6146361d1a
Robert Ancell <robert.ancell@canonical.com>
2021-02-18 11:55:38 +1300

    Add API to get owned names and subscribe to changes
    Fixes https://github.com/canonical/dbus.dart/issues/64

---
+++ example/request_name.dart
@@ -0,0 +1,37 @@
+import 'package:dbus/dbus.dart';
+
+void acquireName(
+    DBusClient client, String name, Set<DBusRequestNameFlag> flags) async {
+  var result = await client.requestName(name, flags: flags);
+  switch (result) {
+    case DBusRequestNameReply.primaryOwner:
+      print('Now the owner of name ${name}');
+      break;
+    case DBusRequestNameReply.inQueue:
+      print('In queue to own name ${name}');
+      break;
+    case DBusRequestNameReply.exists:
+      print('Unable to own name ${name}, already in use');
+      break;
+    case DBusRequestNameReply.alreadyOwner:
+      print('Already the owner of name ${name}');
+      break;
+  }
+}
+
+void main() async {
+  var client = DBusClient.session();
+  client.nameAcquired.listen((name) => print('Acquired name ${name}'));
+  client.nameLost.listen((name) => print('Lost name ${name}'));
+  await acquireName(client, 'com.canonical.DBusDart1', {});
+  await acquireName(client, 'com.canonical.DBusDart2',
+      {DBusRequestNameFlag.allowReplacement});
+  await acquireName(
+      client, 'com.canonical.DBusDart3', {DBusRequestNameFlag.replaceExisting});
+  await acquireName(client, 'com.canonical.DBusDart4', {
+    DBusRequestNameFlag.allowReplacement,
+    DBusRequestNameFlag.replaceExisting
+  });
+
+  print('Currently own names: ${client.ownedNames}');
+}
