canonical/dbus.dart

commit 56150b1a93b24dde8f1a8db28c196d22b38e0eee

Remove unnecessasry braces in string interps

lib/src/dbus_client.dart
@@ -119,9 +119,9 @@ class DBusClient {
       var runtimeDir = Platform.environment['XDG_USER_DIR'];
       if (runtimeDir == null) {
         var uid = getuid();
-        runtimeDir = '/run/user/${uid}';
+        runtimeDir = '/run/user/$uid';
       }
-      address = 'unix:path=${runtimeDir}/bus';
+      address = 'unix:path=$runtimeDir/bus';
     }
     _address = address;
   }
@@ -180,7 +180,7 @@ class DBusClient {
         values: [DBusString(name), DBusUint32(flagsValue)]);
     var values = result.returnValues;
     if (values.length != 1 || values[0].signature != DBusSignature('u')) {
-      throw 'org.freedesktop.DBus.RequestName returned invalid result: ${values}';
+      throw 'org.freedesktop.DBus.RequestName returned invalid result: $values';
     }
     var returnCode = (result.returnValues[0] as DBusUint32).value;
     switch (returnCode) {
@@ -194,7 +194,7 @@ class DBusClient {
       case 4:
         return DBusRequestNameReply.alreadyOwner;
       default:
-        throw 'org.freedesktop.DBusRequestName returned unknown return code: ${returnCode}';
+        throw 'org.freedesktop.DBusRequestName returned unknown return code: $returnCode';
     }
   }
 
@@ -208,7 +208,7 @@ class DBusClient {
         values: [DBusString(name)]);
     var values = result.returnValues;
     if (values.length != 1 || values[0].signature != DBusSignature('u')) {
-      throw 'org.freedesktop.DBus.ReleaseName returned invalid result: ${values}';
+      throw 'org.freedesktop.DBus.ReleaseName returned invalid result: $values';
     }
     var returnCode = (result.returnValues[0] as DBusUint32).value;
     switch (returnCode) {
@@ -220,7 +220,7 @@ class DBusClient {
       case 3:
         return DBusReleaseNameReply.notOwner;
       default:
-        throw 'org.freedesktop.DBus.ReleaseName returned unknown return code: ${returnCode}';
+        throw 'org.freedesktop.DBus.ReleaseName returned unknown return code: $returnCode';
     }
   }
 
@@ -233,7 +233,7 @@ class DBusClient {
         member: 'ListQueuedOwners');
     var values = result.returnValues;
     if (values.length != 1 || values[0].signature != DBusSignature('as')) {
-      throw 'org.freedesktop.DBus.ListQueuedOwners returned invalid result: ${values}';
+      throw 'org.freedesktop.DBus.ListQueuedOwners returned invalid result: $values';
     }
     return (values[0] as DBusArray)
         .children
@@ -250,7 +250,7 @@ class DBusClient {
         member: 'ListNames');
     var values = result.returnValues;
     if (values.length != 1 || values[0].signature != DBusSignature('as')) {
-      throw 'org.freedesktop.DBus.ListNames returned invalid result: ${values}';
+      throw 'org.freedesktop.DBus.ListNames returned invalid result: $values';
     }
     return (values[0] as DBusArray)
         .children
@@ -267,7 +267,7 @@ class DBusClient {
         member: 'ListActivatableNames');
     var values = result.returnValues;
     if (values.length != 1 || values[0].signature != DBusSignature('as')) {
-      throw 'org.freedesktop.DBus.ListActivatableNames returned invalid result: ${values}';
+      throw 'org.freedesktop.DBus.ListActivatableNames returned invalid result: $values';
     }
     return (values[0] as DBusArray)
         .children
@@ -285,7 +285,7 @@ class DBusClient {
         values: [DBusString(name)]);
     var values = result.returnValues;
     if (values.length != 1 || values[0].signature != DBusSignature('b')) {
-      throw 'org.freedesktop.DBus.NameHasOwner returned invalid result: ${values}';
+      throw 'org.freedesktop.DBus.NameHasOwner returned invalid result: $values';
     }
     return (values[0] as DBusBoolean).value;
   }
@@ -304,7 +304,7 @@ class DBusClient {
     }
     var values = result.returnValues;
     if (values.length != 1 || values[0].signature != DBusSignature('s')) {
-      throw 'org.freedesktop.DBus.GetNameOwner returned invalid result: ${values}';
+      throw 'org.freedesktop.DBus.GetNameOwner returned invalid result: $values';
     }
     return (values[0] as DBusString).value;
   }
@@ -318,7 +318,7 @@ class DBusClient {
         member: 'GetId');
     var values = result.returnValues;
     if (values.length != 1 || values[0].signature != DBusSignature('s')) {
-      throw 'org.freedesktop.DBus.GetId returned invalid result: ${values}';
+      throw 'org.freedesktop.DBus.GetId returned invalid result: $values';
     }
     return (values[0] as DBusString).value;
   }
@@ -344,7 +344,7 @@ class DBusClient {
         member: 'GetMachineId');
     var values = result.returnValues;
     if (values.length != 1 || values[0].signature != DBusSignature('s')) {
-      throw 'org.freedesktop.DBus.Peer.GetMachineId returned invalid result: ${values}';
+      throw 'org.freedesktop.DBus.Peer.GetMachineId returned invalid result: $values';
     }
     return (values[0] as DBusString).value;
   }
@@ -415,7 +415,7 @@ class DBusClient {
   Future<void> _openSocket() async {
     var address = DBusAddress.fromString(_address!);
     if (address.transport != 'unix') {
-      throw 'D-Bus address transport not supported: ${_address}';
+      throw 'D-Bus address transport not supported: $_address';
     }
 
     var paths = <String>[];
@@ -425,7 +425,7 @@ class DBusClient {
       }
     }
     if (paths.isEmpty) {
-      throw 'Unable to determine D-Bus unix address path: ${_address}';
+      throw 'Unable to determine D-Bus unix address path: $_address';
     }
 
     var socket_address =
@@ -445,7 +445,7 @@ class DBusClient {
     for (var c in uid.toString().runes) {
       uidString += c.toRadixString(16).padLeft(2, '0');
     }
-    _socket?.write('AUTH EXTERNAL ${uidString}\r\n');
+    _socket?.write('AUTH EXTERNAL $uidString\r\n');
 
     return _authenticateCompleter.future;
   }
@@ -473,7 +473,7 @@ class DBusClient {
         requireConnect: false);
     var values = result.returnValues;
     if (values.length != 1 || values[0].signature != DBusSignature('s')) {
-      throw 'org.freedesktop.DBus.Hello returned invalid result: ${values}';
+      throw 'org.freedesktop.DBus.Hello returned invalid result: $values';
     }
     _uniqueName = (values[0] as DBusString).value;
 
@@ -551,18 +551,18 @@ class DBusClient {
   String _makeMatchRule(String type, String? sender, String? interface,
       String? member, DBusObjectPath? path, DBusObjectPath? pathNamespace) {
     var rules = <String>[];
-    rules.add("type='${type}'");
+    rules.add("type='$type'");
     if (sender != null) {
-      rules.add("sender='${sender}'");
+      rules.add("sender='$sender'");
     }
     if (interface != null) {
-      rules.add("interface='${interface}'");
+      rules.add("interface='$interface'");
     }
     if (member != null) {
-      rules.add("member='${member}'");
+      rules.add("member='$member'");
     }
     if (path != null) {
-      rules.add("path='${path.value}'");
+      rules.add("path='$path.value'");
     }
     if (pathNamespace != null) {
       rules.add("path_namespace='${pathNamespace.value}'");
@@ -582,7 +582,7 @@ class DBusClient {
           values: [DBusString(rule)]);
       var values = result.returnValues;
       if (values.isNotEmpty) {
-        throw 'org.freedesktop.DBus.AddMatch returned invalid result: ${values}';
+        throw 'org.freedesktop.DBus.AddMatch returned invalid result: $values';
       }
       count = 1;
     } else {
@@ -595,7 +595,7 @@ class DBusClient {
   Future<void> _removeMatch(String rule) async {
     var count = _matchRules[rule];
     if (count == null) {
-      throw 'Attempted to remove match that is not added: ${rule}';
+      throw 'Attempted to remove match that is not added: $rule';
     }
 
     if (count == 1) {
@@ -607,7 +607,7 @@ class DBusClient {
           values: [DBusString(rule)]);
       var values = result.returnValues;
       if (values.isNotEmpty) {
-        throw 'org.freedesktop.DBus.RemoveMatch returned invalid result: ${values}';
+        throw 'org.freedesktop.DBus.RemoveMatch returned invalid result: $values';
       }
       _matchRules.remove(rule);
     } else {
@@ -641,7 +641,7 @@ class DBusClient {
       _socket?.write('BEGIN\r\n');
       _authenticateCompleter.complete();
     } else {
-      throw 'Failed to authenticate: ${line}';
+      throw 'Failed to authenticate: $line';
     }
 
     return false;
@@ -861,6 +861,6 @@ class DBusClient {
 
   @override
   String toString() {
-    return "DBusClient('${_address}')";
+    return "DBusClient('$_address')";
   }
 }

bin/dart-dbus.dart
@@ -22,7 +22,7 @@ class GenerateObjectCommand extends Command {
   void run() async {
     if (argResults?.rest.length != 1) {
       usageException(
-          '${name} requires a single D-Bus interface file to be provided.');
+          '$name requires a single D-Bus interface file to be provided.');
     }
     var filename = argResults!.rest[0];
     generateModule(name, argResults?['class-name'], generateObjectClass,
@@ -50,7 +50,7 @@ class GenerateRemoteObjectCommand extends Command {
   void run() async {
     if (argResults?.rest.length != 1) {
       usageException(
-          '${name} requires a single D-Bus interface file to be provided.');
+          '$name requires a single D-Bus interface file to be provided.');
     }
     var filename = argResults!.rest[0];
     generateModule(name, argResults?['class-name'], generateRemoteObjectClass,
@@ -93,7 +93,7 @@ void generateModule(
   var source = '';
   source +=
       '// This file was generated using the following command and may be overwritten.\n';
-  source += '// dart-dbus ${command} ${interfaceFilename}\n';
+  source += '// dart-dbus $command $interfaceFilename\n';
   source += '\n';
   source += "import 'package:dbus/dbus.dart';\n";
   source += '\n';
@@ -106,7 +106,7 @@ void generateModule(
     print(source);
   } else {
     await File(outputFilename).writeAsString(source);
-    print('Wrote to ${outputFilename}');
+    print('Wrote to $outputFilename');
   }
 }
 
@@ -153,7 +153,7 @@ String? generateObjectClass(DBusIntrospectNode node, String className) {
   methods.add(generateGetAllProperties(node));
 
   var source = '';
-  source += 'class ${className} extends DBusObject {\n';
+  source += 'class $className extends DBusObject {\n';
   source += methods.join('\n');
   source += '}\n';
 
@@ -179,7 +179,7 @@ List<String> generatePropertyImplementationMethods(
     var source = '';
     source +=
         '  /// Gets value of property ${interface.name}.${property.name}\n';
-    source += '  Future<DBusMethodResponse> ${methodName}() async {\n';
+    source += '  Future<DBusMethodResponse> $methodName() async {\n';
     source +=
         "    return DBusMethodErrorResponse.failed('Get ${interface.name}.${property.name} not implemented');\n";
     source += '  }\n';
@@ -193,7 +193,7 @@ List<String> generatePropertyImplementationMethods(
     var source = '';
     source += '  /// Sets property ${interface.name}.${property.name}\n';
     source +=
-        '  Future<DBusMethodResponse> ${methodName}(${type.nativeType} value) async {\n';
+        '  Future<DBusMethodResponse> $methodName(${type.nativeType} value) async {\n';
     source +=
         "    return DBusMethodErrorResponse.failed('Set ${interface.name}.${property.name} not implemented');\n";
     source += '  }\n';
@@ -212,9 +212,9 @@ String generateMethodImplementation(List<String> methodNames,
   for (var arg in method.args) {
     if (arg.direction == DBusArgumentDirection.in_) {
       var type = getDartType(arg.type);
-      var argName = arg.name ?? 'arg_${index}';
+      var argName = arg.name ?? 'arg_$index';
       var convertedValue = type.nativeToDBus(argName);
-      argsList.add('${type.nativeType} ${argName}');
+      argsList.add('${type.nativeType} $argName');
       argValues.add(convertedValue);
     }
     index++;
@@ -239,7 +239,7 @@ String generateMethodImplementation(List<String> methodNames,
   var source = '';
   source += '  /// Implementation of ${interface.name}.${method.name}()\n';
   source +=
-      '  Future<DBusMethodResponse> ${methodName}(${argsList.join(', ')}) async {\n';
+      '  Future<DBusMethodResponse> $methodName(${argsList.join(', ')}) async {\n';
   source +=
       "    return DBusMethodErrorResponse.failed('${interface.name}.${method.name}() not implemented');\n";
   source += '  }\n';
@@ -293,12 +293,12 @@ String generateSignalEmitMethod(List<String> methodNames,
   var index = 0;
   for (var arg in signal.args) {
     var type = getDartType(arg.type);
-    var argName = arg.name ?? 'arg_${index}';
+    var argName = arg.name ?? 'arg_$index';
     while (argNames.contains(argName)) {
       argName += '_';
     }
     argNames.add(argName);
-    argsList.add('${type.nativeType} ${argName}');
+    argsList.add('${type.nativeType} $argName');
     var convertedValue = type.nativeToDBus(argName);
     argValues.add(convertedValue);
     index++;
@@ -308,7 +308,7 @@ String generateSignalEmitMethod(List<String> methodNames,
 
   var source = '';
   source += '  /// Emits signal ${interface.name}.${signal.name}\n';
-  source += '  void ${methodName}(${argsList.join(', ')}) {\n';
+  source += '  void $methodName(${argsList.join(', ')}) {\n';
   source +=
       "     emitSignal('${interface.name}', '${signal.name}', [ ${argValues.join(', ')} ]);\n";
   source += '  }\n';
@@ -324,7 +324,7 @@ String generateIntrospectMethod(DBusIntrospectNode node) {
     args.add("'${interface.name}'");
 
     String makeIntrospectObject(String name, Iterable<String> args) {
-      return '${name}(${args.join(', ')})';
+      return '$name(${args.join(', ')})';
     }
 
     String makeArg(DBusIntrospectArgument arg) {
@@ -412,8 +412,8 @@ String generateHandleMethodCall(DBusIntrospectNode node) {
       for (var arg in method.args) {
         if (arg.direction == DBusArgumentDirection.in_) {
           var argName = 'values[${argValues.length}]';
-          argChecks.add(
-              "${argName}.signature != DBusSignature('${arg.type.value}')");
+          argChecks
+              .add("$argName.signature != DBusSignature('${arg.type.value}')");
           var type = getDartType(arg.type);
           var convertedValue = type.dbusToNative(argName);
           argValues.add(convertedValue);
@@ -458,7 +458,7 @@ String generateGetProperty(
       if (property.access == DBusPropertyAccess.readwrite ||
           property.access == DBusPropertyAccess.read) {
         var methodName = getMethodNames['${interface.name}.${property.name}'];
-        source += 'return ${methodName}();\n';
+        source += 'return $methodName();\n';
       } else {
         source = 'return DBusMethodErrorResponse.propertyWriteOnly()\n';
       }
@@ -502,7 +502,7 @@ String generateSetProperty(
           property.access == DBusPropertyAccess.write) {
         var convertedValue = type.dbusToNative('value');
         var methodName = setMethodNames['${interface.name}.${property.name}'];
-        source += 'return ${methodName}(${convertedValue});\n';
+        source += 'return $methodName($convertedValue);\n';
       } else {
         source = 'return DBusMethodErrorResponse.propertyReadOnly()\n';
       }
@@ -538,7 +538,7 @@ String generateGetAllProperties(DBusIntrospectNode node) {
           property.access == DBusPropertyAccess.read) {
         var convertedValue = '(await get${property.name}()).returnValues[0]';
         source +=
-            "properties[DBusString('${property.name}')] = ${convertedValue};\n";
+            "properties[DBusString('${property.name}')] = $convertedValue;\n";
       }
     }
     if (source != '') {
@@ -602,9 +602,9 @@ String? generateRemoteObjectClass(DBusIntrospectNode node, String className) {
   }
 
   var source = '';
-  source += 'class ${className} extends DBusRemoteObject {\n';
+  source += 'class $className extends DBusRemoteObject {\n';
   source +=
-      '''  ${className}(DBusClient client, String destination, ${pathArg}) : super(client, destination, path);\n''';
+      '''  $className(DBusClient client, String destination, $pathArg) : super(client, destination, path);\n''';
   source += '\n';
   source += methods.join('\n');
   source += '}\n';
@@ -627,10 +627,10 @@ List<String> generateRemotePropertyMethods(List<String> methodNames,
     var convertedValue = type.dbusToNative('value');
     var source = '';
     source += '  /// Gets ${interface.name}.${property.name}\n';
-    source += '  Future<${type.nativeType}> ${methodName}() async {\n';
+    source += '  Future<${type.nativeType}> $methodName() async {\n';
     source +=
         "    var value = await getProperty('${interface.name}', '${property.name}');\n";
-    source += '    return ${convertedValue};\n';
+    source += '    return $convertedValue;\n';
     source += '  }\n';
     methods.add(source);
   }
@@ -642,10 +642,9 @@ List<String> generateRemotePropertyMethods(List<String> methodNames,
     var convertedValue = type.nativeToDBus('value');
     var source = '';
     source += '  /// Sets ${interface.name}.${property.name}\n';
+    source += '  Future<void> $methodName (${type.nativeType} value) async {\n';
     source +=
-        '  Future<void> ${methodName} (${type.nativeType} value) async {\n';
-    source +=
-        "    await setProperty('${interface.name}', '${property.name}', ${convertedValue});\n";
+        "    await setProperty('${interface.name}', '${property.name}', $convertedValue);\n";
     source += '  }\n';
     methods.add(source);
   }
@@ -662,9 +661,9 @@ String generateRemoteMethodCall(List<String> methodNames,
   for (var arg in method.args) {
     if (arg.direction == DBusArgumentDirection.in_) {
       var type = getDartType(arg.type);
-      var argName = arg.name ?? 'arg_${index}';
+      var argName = arg.name ?? 'arg_$index';
       var convertedValue = type.nativeToDBus(argName);
-      argsList.add('${type.nativeType} ${argName}');
+      argsList.add('${type.nativeType} $argName');
       argValues.add(convertedValue);
     }
     index++;
@@ -700,14 +699,14 @@ String generateRemoteMethodCall(List<String> methodNames,
 
   var source = '';
   source += '  /// Invokes ${interface.name}.${method.name}()\n';
-  source += '  ${returnType} ${methodName}(${argsList.join(', ')}) async {\n';
+  source += '  $returnType $methodName(${argsList.join(', ')}) async {\n';
   if (returnTypes.isEmpty) {
-    source += '    ${methodCall}\n';
+    source += '    $methodCall\n';
   } else if (returnTypes.length == 1) {
-    source += '    var result = ${methodCall}\n';
+    source += '    var result = $methodCall\n';
     source += '    return ${returnValues[0]};\n';
   } else {
-    source += '    var result = ${methodCall}\n';
+    source += '    var result = $methodCall\n';
     source += '    return result.returnValues;\n';
   }
   source += '  }\n';
@@ -769,27 +768,26 @@ String generateRemoteSignalClass(String classPrefix,
     var type = getDartType(arg.type);
 
     // Modify the arg name if it collides.
-    var argName = arg.name ?? 'arg_${index}';
+    var argName = arg.name ?? 'arg_$index';
     while (argNames.contains(argName)) {
       argName += '_';
     }
     argNames.add(argName);
 
-    var valueName = 'values[${index}]';
+    var valueName = 'values[$index]';
     var convertedValue = type.dbusToNative(valueName);
-    properties
-        .add('  ${type.nativeType} get ${argName} => ${convertedValue};\n');
-    params.add('this.${argName}');
+    properties.add('  ${type.nativeType} get $argName => $convertedValue;\n');
+    params.add('this.$argName');
     index++;
   }
 
   var source = '';
   source += '/// Signal data for ${interface.name}.${signal.name}.\n';
-  source += 'class ${classPrefix}${signal.name} extends DBusSignal{\n';
+  source += 'class $classPrefix${signal.name} extends DBusSignal{\n';
   source += properties.join();
   source += '\n';
   source +=
-      '  ${classPrefix}${signal.name}(DBusSignal signal) : super(signal.sender, signal.path, signal.interface, signal.member, signal.values);\n';
+      '  $classPrefix${signal.name}(DBusSignal signal) : super(signal.sender, signal.path, signal.interface, signal.member, signal.values);\n';
   source += '}\n';
 
   return source;
@@ -809,9 +807,9 @@ String generateRemoteSignalSubscription(
     valueChecks.add('signal.values.length == ${signal.args.length}');
   }
   for (var arg in signal.args) {
-    var valueName = 'signal.values[${index}]';
+    var valueName = 'signal.values[$index]';
     valueChecks
-        .add("${valueName}.signature == DBusSignature('${arg.type.value}')");
+        .add("$valueName.signature == DBusSignature('${arg.type.value}')");
     index++;
   }
 
@@ -819,12 +817,12 @@ String generateRemoteSignalSubscription(
 
   var source = '';
   source += '  /// Subscribes to ${interface.name}.${signal.name}.\n';
-  source += '  Stream<${classPrefix}${signal.name}> ${methodName}() async* {\n';
+  source += '  Stream<$classPrefix${signal.name}> $methodName() async* {\n';
   source +=
       "    var signals = await subscribeSignal('${interface.name}', '${signal.name}');\n";
   source += '    await for (var signal in signals) {\n';
   source += '      if (${valueChecks.join(' && ')}) {\n';
-  source += '        yield ${classPrefix}${signal.name}(signal);\n';
+  source += '        yield $classPrefix${signal.name}(signal);\n';
   source += '      }\n';
   source += '    }\n';
   source += '  }\n';
@@ -877,7 +875,7 @@ String makeSwitch(Iterable<SwitchBranch> branches, [String? defaultBranch]) {
   var isFirst = true;
   for (var branch in branches) {
     var statement = isFirst ? 'if' : '} else if';
-    source += '${statement} (${branch.condition}) {\n';
+    source += '$statement (${branch.condition}) {\n';
     source += indentSource(1, branch.source);
     isFirst = false;
   }

example/object_manager.dart
@@ -11,7 +11,7 @@ void main() async {
       printInterfacesAndProperties(signal.interfacesAndProperties);
     } else if (signal is DBusObjectManagerInterfacesRemovedSignal) {
       for (var interface in signal.interfaces) {
-        print('${signal.changedPath.value} removed interfaces ${interface}');
+        print('${signal.changedPath.value} removed interfaces $interface');
       }
     } else if (signal is DBusPropertiesChangedSignal) {
       print('${signal.path.value}');
@@ -30,9 +30,9 @@ void main() async {
 void printInterfacesAndProperties(
     Map<String, Map<String, DBusValue>> interfacesAndProperties) {
   interfacesAndProperties.forEach((interface, properties) {
-    print('  ${interface}');
+    print('  $interface');
     properties.forEach((name, value) {
-      print('    ${name}: ${value.toNative()}');
+      print('    $name: ${value.toNative()}');
     });
   });
 }

example/properties.dart
@@ -8,7 +8,7 @@ void main() async {
   var properties =
       await object.getAllProperties('org.freedesktop.NetworkManager');
   properties.forEach((name, value) {
-    print('${name}: ${value.toNative()}');
+    print('$name: ${value.toNative()}');
   });
 
   print('');
@@ -26,7 +26,7 @@ void main() async {
 
   object.subscribePropertiesChanged().listen((signal) {
     signal.changedProperties.forEach((name, value) {
-      print('${name}: ${value.toNative()}');
+      print('$name: ${value.toNative()}');
     });
   });
 }

example/request_name.dart
@@ -5,24 +5,24 @@ Future<void> acquireName(
   var result = await client.requestName(name, flags: flags);
   switch (result) {
     case DBusRequestNameReply.primaryOwner:
-      print('Now the owner of name ${name}');
+      print('Now the owner of name $name');
       break;
     case DBusRequestNameReply.inQueue:
-      print('In queue to own name ${name}');
+      print('In queue to own name $name');
       break;
     case DBusRequestNameReply.exists:
-      print('Unable to own name ${name}, already in use');
+      print('Unable to own name $name, already in use');
       break;
     case DBusRequestNameReply.alreadyOwner:
-      print('Already the owner of name ${name}');
+      print('Already the owner of name $name');
       break;
   }
 }
 
 void main() async {
   var client = DBusClient.session();
-  client.nameAcquired.listen((name) => print('Acquired name ${name}'));
-  client.nameLost.listen((name) => print('Lost name ${name}'));
+  client.nameAcquired.listen((name) => print('Acquired name $name'));
+  client.nameLost.listen((name) => print('Lost name $name'));
   await acquireName(client, 'com.canonical.DBusDart1', {});
   await acquireName(client, 'com.canonical.DBusDart2',
       {DBusRequestNameFlag.allowReplacement});

example/signals.dart
@@ -35,7 +35,7 @@ void main(List<String> args) async {
     var signals = object.subscribeSignal('com.canonical.DBusDart', 'Ping');
     await for (var signal in signals) {
       var count = (signal.values[0] as DBusUint64).value;
-      print('Ping ${count}!');
+      print('Ping $count!');
     }
   } else if (mode == 'server') {
     await client.requestName('com.canonical.DBusDart');
@@ -43,7 +43,7 @@ void main(List<String> args) async {
     client.registerObject(object);
     var count = 0;
     Timer.periodic(Duration(seconds: 1), (timer) {
-      print('Ping ${count}!');
+      print('Ping $count!');
       object.emitSignal('com.canonical.DBusDart', 'Ping', [DBusUint64(count)]);
       count++;
     });

lib/src/dbus_address.dart
@@ -27,7 +27,7 @@ class DBusAddress {
     // Addresses are in the form 'transport:key1=value1,key2=value2'
     var index = address.indexOf(':');
     if (index < 0) {
-      throw 'Unable to determine transport of D-Bus address: ${address}';
+      throw 'Unable to determine transport of D-Bus address: $address';
     }
 
     var transport = address.substring(0, index);
@@ -45,13 +45,13 @@ class DBusAddress {
     for (var property in propertiesList.split(',')) {
       var index = property.indexOf('=');
       if (index < 0) {
-        throw 'Invalid D-Bus address property: ${property}';
+        throw 'Invalid D-Bus address property: $property';
       }
 
       var key = property.substring(0, index);
       var value = _decodeValue(property.substring(index + 1));
       if (value == null) {
-        throw 'Invalid value in D-Bus address property: ${property}';
+        throw 'Invalid value in D-Bus address property: $property';
       }
 
       properties.add(DBusAddressProperty(key, value));

commit 947cf5d5f0d888d92f4aacc3a6ea20848a066773

Fix regression subcribing to signals.
The DBus match rule code generator was broken in 56150b1a93b24dde8f1a8db28c196d22b38e0eee

lib/src/dbus_client.dart
@@ -566,7 +566,7 @@ class DBusClient {
       rules.add("member='$member'");
     }
     if (path != null) {
-      rules.add("path='$path.value'");
+      rules.add("path='${path.value}'");
     }
     if (pathNamespace != null) {
       rules.add("path_namespace='${pathNamespace.value}'");