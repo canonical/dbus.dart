55b1c68b307bd35b381bdbc7aa73ed71b1d81cb6
Robert Ancell <robert.ancell@canonical.com>
2021-03-18 12:41:53 +1300

    Implement a name queue in DBusServer

--- test/test.dart
+++ test/test.dart
@@ -25,25 +25,305 @@ class IntrospectObject extends DBusObject {
 }
 
 void main() {
-  test('ping server', () async {
+  test('ping', () async {
     var server = DBusServer();
     var address = await server.listenUnixSocket();
+    var client = DBusClient(address);
 
     // Check can ping the server.
-    var client = DBusClient(address);
-    await client.ping('org.freedesktop.DBus');
+    await client.ping();
   });
 
-  test('method call', () async {
+  test('list names', () async {
     var server = DBusServer();
     var address = await server.listenUnixSocket();
+    var client = DBusClient(address);
+
+    // Check the server and this clients name is reported.
+    var names = await client.listNames();
+    expect(names, equals(['org.freedesktop.DBus', client.uniqueName]));
+  });
+
+  test('list activatable names', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client = DBusClient(address);
+
+    // Not currently supported, so should be empty.
+    var names = await client.listActivatableNames();
+    expect(names, isEmpty);
+  });
+
+  test('request name', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client = DBusClient(address);
+
+    // Check name is currently unowned.
+    var hasOwner = await client.nameHasOwner('com.example.Test');
+    expect(hasOwner, isFalse);
+    var names = await client.listQueuedOwners('com.example.Test');
+    expect(names, isEmpty);
+
+    // Check get an event when acquired.
+    client.nameAcquired.listen(expectAsync1((name) {
+      expect(name, equals('com.example.Test'));
+    }));
+
+    // Request the name.
+    var reply = await client.requestName('com.example.Test');
+    expect(reply, equals(DBusRequestNameReply.primaryOwner));
+
+    // Check name is owned.
+    expect(client.ownedNames, equals(['com.example.Test']));
+    names = await client.listNames();
+    expect(
+        names,
+        equals(
+            ['org.freedesktop.DBus', client.uniqueName, 'com.example.Test']));
+    hasOwner = await client.nameHasOwner('com.example.Test');
+    expect(hasOwner, isTrue);
+    var owner = await client.getNameOwner('com.example.Test');
+    expect(owner, equals(client.uniqueName));
+    names = await client.listQueuedOwners('com.example.Test');
+    expect(names, [client.uniqueName]);
+  });
+
+  test('request name - already owned', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client = DBusClient(address);
+
+    // Request the name twice
+    var reply = await client.requestName('com.example.Test');
+    expect(reply, equals(DBusRequestNameReply.primaryOwner));
+    reply = await client.requestName('com.example.Test');
+    expect(reply, equals(DBusRequestNameReply.alreadyOwner));
+
+    // Check name is owned only once.
+    var hasOwner = await client.nameHasOwner('com.example.Test');
+    expect(hasOwner, isTrue);
+    var owner = await client.getNameOwner('com.example.Test');
+    expect(owner, equals(client.uniqueName));
+    var names = await client.listQueuedOwners('com.example.Test');
+    expect(names, [client.uniqueName]);
+  });
+
+  test('request name - queue', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client1 = DBusClient(address);
+    var client2 = DBusClient(address);
+
+    // Own a name with one client.
+    var reply = await client1.requestName('com.example.Test');
+    expect(reply, equals(DBusRequestNameReply.primaryOwner));
+
+    // Attempt to replace the name with another client.
+    reply = await client2.requestName('com.example.Test');
+    expect(reply, equals(DBusRequestNameReply.inQueue));
+
+    // Check name is correctly owned and second client is in queue.
+    var hasOwner = await client1.nameHasOwner('com.example.Test');
+    expect(hasOwner, isTrue);
+    var owner = await client1.getNameOwner('com.example.Test');
+    expect(owner, equals(client1.uniqueName));
+    var names = await client1.listQueuedOwners('com.example.Test');
+    expect(names, equals([client1.uniqueName, client2.uniqueName]));
+  });
+
+  test('request name - do not queue', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client1 = DBusClient(address);
+    var client2 = DBusClient(address);
+
+    // Own a name with one client.
+    var reply = await client1.requestName('com.example.Test');
+    expect(reply, equals(DBusRequestNameReply.primaryOwner));
+
+    // Attempt to replace the name with another client.
+    reply = await client2.requestName('com.example.Test',
+        flags: {DBusRequestNameFlag.doNotQueue});
+    expect(reply, equals(DBusRequestNameReply.exists));
+
+    // Check name is correctly owned and second client is not in queue.
+    var hasOwner = await client1.nameHasOwner('com.example.Test');
+    expect(hasOwner, isTrue);
+    var owner = await client1.getNameOwner('com.example.Test');
+    expect(owner, equals(client1.uniqueName));
+    var names = await client1.listQueuedOwners('com.example.Test');
+    expect(names, equals([client1.uniqueName]));
+  });
+
+  test('request name - replace', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client1 = DBusClient(address);
+    var client2 = DBusClient(address);
+
+    // Own a name with one client.
+    var reply = await client1.requestName('com.example.Test',
+        flags: {DBusRequestNameFlag.allowReplacement});
+    expect(reply, equals(DBusRequestNameReply.primaryOwner));
+
+    // Replace the name with another client.
+    reply = await client2.requestName('com.example.Test',
+        flags: {DBusRequestNameFlag.replaceExisting});
+    expect(reply, equals(DBusRequestNameReply.primaryOwner));
+
+    // Check name is correctly owned and second client is in queue.
+    var hasOwner = await client1.nameHasOwner('com.example.Test');
+    expect(hasOwner, isTrue);
+    var owner = await client1.getNameOwner('com.example.Test');
+    expect(owner, equals(client2.uniqueName));
+    var names = await client1.listQueuedOwners('com.example.Test');
+    expect(names, equals([client2.uniqueName, client1.uniqueName]));
+  });
+
+  test('request name - replace, do not queue', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client1 = DBusClient(address);
+    var client2 = DBusClient(address);
+
+    // Own a name with one client.
+    var reply = await client1.requestName('com.example.Test', flags: {
+      DBusRequestNameFlag.allowReplacement,
+      DBusRequestNameFlag.doNotQueue
+    });
+    expect(reply, equals(DBusRequestNameReply.primaryOwner));
+
+    // Replace the name with another client.
+    reply = await client2.requestName('com.example.Test',
+        flags: {DBusRequestNameFlag.replaceExisting});
+    expect(reply, equals(DBusRequestNameReply.primaryOwner));
+
+    // Check name is correctly owned and first client is not in queue.
+    var hasOwner = await client1.nameHasOwner('com.example.Test');
+    expect(hasOwner, isTrue);
+    var owner = await client1.getNameOwner('com.example.Test');
+    expect(owner, equals(client2.uniqueName));
+    var names = await client1.listQueuedOwners('com.example.Test');
+    expect(names, equals([client2.uniqueName]));
+  });
+
+  test('request name - replace not allowed', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client1 = DBusClient(address);
+    var client2 = DBusClient(address);
+
+    // Own a name with one client.
+    var reply = await client1.requestName('com.example.Test');
+    expect(reply, equals(DBusRequestNameReply.primaryOwner));
+
+    // Attempt to replace the name with another client.
+    reply = await client2.requestName('com.example.Test',
+        flags: {DBusRequestNameFlag.replaceExisting});
+    expect(reply, equals(DBusRequestNameReply.inQueue));
+
+    // Check name is correctly owned and second client is in queue.
+    var hasOwner = await client1.nameHasOwner('com.example.Test');
+    expect(hasOwner, isTrue);
+    var owner = await client1.getNameOwner('com.example.Test');
+    expect(owner, equals(client1.uniqueName));
+    var names = await client1.listQueuedOwners('com.example.Test');
+    expect(names, equals([client1.uniqueName, client2.uniqueName]));
+  });
+
+  test('release name', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client = DBusClient(address);
+
+    // Check get an event when acquired and lost
+    client.nameAcquired.listen(expectAsync1((name) {
+      expect(name, equals('com.example.Test'));
+    }));
+    client.nameLost.listen(expectAsync1((name) {
+      expect(name, equals('com.example.Test'));
+    }));
+
+    // Request the name.
+    var requestReply = await client.requestName('com.example.Test');
+    expect(requestReply, equals(DBusRequestNameReply.primaryOwner));
+
+    // Release the name.
+    var releaseReply = await client.releaseName('com.example.Test');
+    expect(releaseReply, equals(DBusReleaseNameReply.released));
+
+    // Check name is unowned.
+    var hasOwner = await client.nameHasOwner('com.example.Test');
+    expect(hasOwner, isFalse);
+    var names = await client.listQueuedOwners('com.example.Test');
+    expect(names, isEmpty);
+  });
+
+  test('release name - non existant', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client = DBusClient(address);
+
+    // Release a name that's not in use.
+    var releaseReply = await client.releaseName('com.example.Test');
+    expect(releaseReply, equals(DBusReleaseNameReply.nonExistant));
+  });
+
+  test('release name - not owner', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client1 = DBusClient(address);
+    var client2 = DBusClient(address);
+
+    // Own a name with one client.
+    var requestReply = await client1.requestName('com.example.Test',
+        flags: {DBusRequestNameFlag.allowReplacement});
+    expect(requestReply, equals(DBusRequestNameReply.primaryOwner));
+
+    // Attempt to release that name from another client.
+    var releaseReply = await client2.releaseName('com.example.Test');
+    expect(releaseReply, equals(DBusReleaseNameReply.notOwner));
+  });
+
+  test('release name - queue', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client1 = DBusClient(address);
+    var client2 = DBusClient(address);
+
+    // Own a name with one client.
+    var requestReply = await client1.requestName('com.example.Test');
+    expect(requestReply, equals(DBusRequestNameReply.primaryOwner));
+
+    // Join queue for this name.
+    requestReply = await client2.requestName('com.example.Test',
+        flags: {DBusRequestNameFlag.replaceExisting});
+    expect(requestReply, equals(DBusRequestNameReply.inQueue));
+
+    // Have the first client release the name.
+    var releaseReply = await client1.releaseName('com.example.Test');
+    expect(releaseReply, equals(DBusReleaseNameReply.released));
+
+    // Check name is correctly transferred to second client..
+    var hasOwner = await client1.nameHasOwner('com.example.Test');
+    expect(hasOwner, isTrue);
+    var owner = await client1.getNameOwner('com.example.Test');
+    expect(owner, equals(client2.uniqueName));
+    var names = await client1.listQueuedOwners('com.example.Test');
+    expect(names, equals([client2.uniqueName]));
+  });
+
+  test('call method', () async {
+    var server = DBusServer();
+    var address = await server.listenUnixSocket();
+    var client1 = DBusClient(address);
+    var client2 = DBusClient(address);
 
     // Create a client that exposes a method.
-    var client1 = DBusClient(address);
     await client1.registerObject(EchoObject());
 
     // Call the method from another client.
-    var client2 = DBusClient(address);
     var response = await client2.callMethod(
         destination: client1.uniqueName,
         path: DBusObjectPath('/'),
@@ -57,14 +337,14 @@ void main() {
   test('emit signal', () async {
     var server = DBusServer();
     var address = await server.listenUnixSocket();
+    var client1 = DBusClient(address);
+    var client2 = DBusClient(address);
 
     // Create a client that exposes a method.
-    var client1 = DBusClient(address);
     var object = DBusObject();
     await client1.registerObject(object);
 
     // Subscribe to the signal from another client.
-    var client2 = DBusClient(address);
     var signals =
         client2.subscribeSignals(interface: 'com.example.Test', member: 'Ping');
     signals.listen(expectAsync1((signal) {
@@ -86,13 +366,13 @@ void main() {
   test('introspect', () async {
     var server = DBusServer();
     var address = await server.listenUnixSocket();
+    var client1 = DBusClient(address);
+    var client2 = DBusClient(address);
 
     // Create a client that exposes introspection data.
-    var client1 = DBusClient(address);
     await client1.registerObject(IntrospectObject());
 
     // Read introspection data from the first client.
-    var client2 = DBusClient(address);
     var remoteObject =
         DBusRemoteObject(client2, client1.uniqueName!, DBusObjectPath('/'));
     var node = await remoteObject.introspect();
