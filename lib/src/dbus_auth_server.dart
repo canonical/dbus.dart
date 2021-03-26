canonical/dbus.dart

commit d814ef1c3325e1e078a5f6bd8e7a61086c058c03

Split DBusServer authentication into a class

lib/src/dbus_auth_server.dart
@@ -1,0 +1,114 @@
+import 'dart:async';
+
+import 'dbus_uuid.dart';
+
+/// A server for D-Bus authentication.
+class DBusAuthServer {
+  final _responsesController = StreamController<String>();
+  bool _readSocketControlMessage = false;
+  var _externalDone = false;
+  var _isAuthenticated = false;
+
+  /// Unique ID for this connection.
+  final DBusUUID uuid;
+
+  /// Stream of responses to send to the authentication client.
+  Stream<String> get responses => _responsesController.stream;
+
+  /// True if was successfully authenticated.
+  bool get isAuthenticated => _isAuthenticated;
+
+  /// Creates a new authentication server.
+  DBusAuthServer(this.uuid);
+
+  /// Process a request [message] received from the client.
+  void processRequest(String message) {
+    // Skip the empty byte sent if the client used a socket control message to send credentials.
+    if (!_readSocketControlMessage) {
+      message = message.substring(1);
+      _readSocketControlMessage = true;
+    }
+
+    // Validate is ASCII.
+    if (message.contains(RegExp(r'[^\x00-\x7F]+'))) {
+      _error('Message contains non-ASCII characters');
+      return;
+    }
+
+    // First word is a command.
+    var values = _splitOnSpace(message);
+    var command = values[0];
+    var args = values[1];
+
+    switch (command) {
+      case 'AUTH':
+        if (args == '') {
+          // Respond with the mechanisms we support
+          _reject('EXTERNAL');
+        } else {
+          var values = _splitOnSpace(args);
+          var mechanism = values[0];
+          var initialResponse = values[1];
+          switch (mechanism) {
+            case 'EXTERNAL':
+              _authenticateExternal(initialResponse);
+              break;
+            default:
+              _reject("Mechanism '$mechanism' not supported");
+              break;
+          }
+        }
+        break;
+      case 'CANCEL':
+        _reject('Cancelled');
+        break;
+      case 'BEGIN':
+        if (_externalDone) {
+          _isAuthenticated = true;
+        }
+        break;
+      case 'DATA':
+        _reject('Unable to handle DATA command');
+        break;
+      case 'ERROR':
+        _reject('Received error');
+        break;
+      case 'NEGOTIATE_UNIX_FD':
+        break;
+      default:
+        _error("Unknown command '$command'");
+        break;
+    }
+  }
+
+  /// Sends a [message] to the server.
+  void _send(String message) {
+    _responsesController.add(message);
+  }
+
+  /// Rejects authentication.
+  void _reject(String message) {
+    _send('REJECTED $message');
+  }
+
+  /// Sends an error to the client.
+  void _error(String message) {
+    _send('ERROR $message');
+  }
+
+  /// Do authentication using the EXTERNAL mechanism.
+  void _authenticateExternal(String uid) {
+    // Note uid isn't checked.
+    _externalDone = true;
+    _send('OK ${uuid.toHexString()}');
+  }
+
+  List<String> _splitOnSpace(String value) {
+    var index = value.indexOf(' ');
+    if (index >= 0) {
+      return [value.substring(0, index), value.substring(index + 1)];
+    } else {
+      return [value, ''];
+    }
+  }
+}

lib/src/dbus_server.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
-import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:pedantic/pedantic.dart';
 
+import 'dbus_auth_server.dart';
 import 'dbus_bus_name.dart';
 import 'dbus_introspect.dart';
 import 'dbus_introspectable.dart';
@@ -51,9 +51,8 @@ class _DBusRemoteClient {
   /// Incoming data.
   final _readBuffer = DBusReadBuffer();
 
-  /// True once authentication is complete.
-  bool isAuthenticated = false;
-  bool readSocketControlMessage = false;
+  /// Authentication server.
+  final DBusAuthServer _authServer;
 
   /// True when have received a Hello message.
   bool receivedHello = false;
@@ -64,7 +63,9 @@ class _DBusRemoteClient {
   /// Message match rules.
   final matchRules = <DBusMatchRule>[];
 
-  _DBusRemoteClient(this.serverSocket, this._socket, this.uniqueName) {
+  _DBusRemoteClient(this.serverSocket, this._socket, this.uniqueName)
+      : _authServer = DBusAuthServer(serverSocket.uuid) {
+    _authServer.responses.listen((message) => _socket.write(message + '\r\n'));
     _socket.listen(_processData);
   }
 
@@ -102,7 +103,7 @@ class _DBusRemoteClient {
 
     var complete = false;
     while (!complete) {
-      if (!isAuthenticated) {
+      if (!_authServer.isAuthenticated) {
         complete = _processAuth();
       } else {
         complete = _processMessages();
@@ -111,62 +112,14 @@ class _DBusRemoteClient {
     }
   }
 
-  /// Send an authentication response to the client.
-  void _writeAuthResponse(String message) {
-    _socket.write(message + '\r\n');
-  }
-
   /// Processes authentication messages received from the D-Bus client.
   bool _processAuth() {
-    // Skip the empty byte sent if the client used a socket control message to send credentials.
-    if (!readSocketControlMessage) {
-      _readBuffer.readByte();
-      readSocketControlMessage = true;
-    }
-
     var line = _readBuffer.readLine();
     if (line == null) {
       return true;
     }
 
-    var words = line.split(' ');
-    var command = words.isEmpty ? '' : words[0];
-    var args = words.skip(1).toList();
-    switch (command) {
-      case 'AUTH':
-        if (args.isEmpty) {
-          /// Respond with the mechanisms we support
-          _writeAuthResponse('REJECTED EXTERNAL');
-        } else {
-          var mechanism = args[0];
-          if (mechanism == 'EXTERNAL' && args.length == 2) {
-            //var uid = args[1];
-            _writeAuthResponse('OK ${serverSocket.uuid.toHexString()}');
-          } else {
-            _writeAuthResponse('REJECTED');
-          }
-        }
-        break;
-      case 'CANCEL':
-        _writeAuthResponse('REJECTED');
-        break;
-      case 'BEGIN':
-        isAuthenticated = true;
-        break;
-      case 'DATA':
-        _writeAuthResponse('REJECTED');
-        break;
-      case 'ERROR':
-        _writeAuthResponse('REJECTED');
-        break;
-      case 'NEGOTIATE_UNIX_FD':
-        _writeAuthResponse('ERROR Unix fd not supported');
-        break;
-      default:
-        _writeAuthResponse('ERROR Unknown command');
-        break;
-    }
-
+    _authServer.processRequest(line);
     return false;
   }
 

commit b067a8a807bf33cac78cbcf31bffb40c17e14549

Fix server authentication failing with Unix FD negotiation
Broken in d814ef1c3325e1e078a5f6bd8e7a61086c058c03

lib/src/dbus_auth_server.dart
@@ -74,6 +74,7 @@ class DBusAuthServer {
         _reject('Received error');
         break;
       case 'NEGOTIATE_UNIX_FD':
+        _error('Unix fd not supported');
         break;
       default:
         _error("Unknown command '$command'");