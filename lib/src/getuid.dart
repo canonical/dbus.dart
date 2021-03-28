35452ee9af468d7262f3d3dffe8ad33d5ddff1cc
Robert Ancell <robert.ancell@canonical.com>
2021-03-29 09:28:28 +1300

    Conditionally import the FFI version of getuid, so this doesn't break web builds

--- lib/src/getuid.dart
+++ lib/src/getuid.dart
@@ -1,11 +1,2 @@
-import 'dart:ffi';
-
-typedef _getuidC = Int32 Function();
-typedef _getuidDart = int Function();
-
-/// Gets the user ID of the current user.
-int getuid() {
-  final dylib = DynamicLibrary.open('libc.so.6');
-  final getuidP = dylib.lookupFunction<_getuidC, _getuidDart>('getuid');
-  return getuidP();
-}
+// Conditionally import the FFI version, so this doesn't break web builds.
+export 'getuid_stub.dart' if (dart.library.ffi) 'getuid_linux.dart';
