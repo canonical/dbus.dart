canonical/dbus.dart

commit 590fe7086cba1dea8390e4c0800a3aacb0b24df2

Add a signature property to DBusMethodResponse, DBusSignal.
Simplify input checking with this.

lib/src/dbus_remote_object.dart
@@ -71,11 +71,10 @@ class DBusRemoteObject {
         path: path,
         interface: 'org.freedesktop.DBus.Introspectable',
         member: 'Introspect');
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('s')) {
-      throw 'org.freedesktop.DBus.Introspectable.Introspect returned invalid result: $values';
+    if (result.signature != DBusSignature('s')) {
+      throw 'org.freedesktop.DBus.Introspectable.Introspect returned invalid result: ${result.returnValues}';
     }
-    var xml = (values[0] as DBusString).value;
+    var xml = (result.returnValues[0] as DBusString).value;
     return parseDBusIntrospectXml(xml);
   }
 
@@ -87,11 +86,10 @@ class DBusRemoteObject {
         interface: 'org.freedesktop.DBus.Properties',
         member: 'Get',
         values: [DBusString(interface), DBusString(name)]);
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('v')) {
-      throw 'org.freedesktop.DBus.Properties.Get returned invalid result: $values';
+    if (result.signature != DBusSignature('v')) {
+      throw 'org.freedesktop.DBus.Properties.Get returned invalid result: ${result.returnValues}';
     }
-    return (values[0] as DBusVariant).value;
+    return (result.returnValues[0] as DBusVariant).value;
   }
 
   /// Gets the values of all the properties on this object.
@@ -102,11 +100,10 @@ class DBusRemoteObject {
         interface: 'org.freedesktop.DBus.Properties',
         member: 'GetAll',
         values: [DBusString(interface)]);
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('a{sv}')) {
-      throw 'org.freedesktop.DBus.Properties.GetAll returned invalid result: $values';
+    if (result.signature != DBusSignature('a{sv}')) {
+      throw 'org.freedesktop.DBus.Properties.GetAll returned invalid result: ${result.returnValues}';
     }
-    return (values[0] as DBusDict).children.map((key, value) =>
+    return (result.returnValues[0] as DBusDict).children.map((key, value) =>
         MapEntry((key as DBusString).value, (value as DBusVariant).value));
   }
 
@@ -119,9 +116,8 @@ class DBusRemoteObject {
         interface: 'org.freedesktop.DBus.Properties',
         member: 'Set',
         values: [DBusString(interface), DBusString(name), DBusVariant(value)]);
-    var values = result.returnValues;
-    if (values.isNotEmpty) {
-      throw 'org.freedesktop.DBus.Properties.set returned invalid result: $values';
+    if (result.returnValues.isNotEmpty) {
+      throw 'org.freedesktop.DBus.Properties.set returned invalid result: ${result.returnValues}';
     }
   }
 
@@ -130,10 +126,7 @@ class DBusRemoteObject {
     var signals =
         subscribeSignal('org.freedesktop.DBus.Properties', 'PropertiesChanged');
     return signals.map((signal) {
-      if (signal.values.length == 3 &&
-          signal.values[0].signature == DBusSignature('s') &&
-          signal.values[1].signature == DBusSignature('a{sv}') &&
-          signal.values[2].signature == DBusSignature('as')) {
+      if (signal.signature != DBusSignature('sa{sv}as')) {
         return DBusPropertiesChangedSignal(signal);
       } else {
         throw 'org.freedesktop.DBus.Properties.PropertiesChanged contains invalid values ${signal.values}';
@@ -167,10 +160,8 @@ class DBusRemoteObject {
         path: path,
         interface: 'org.freedesktop.DBus.ObjectManager',
         member: 'GetManagedObjects');
-    var values = result.returnValues;
-    if (values.length != 1 ||
-        values[0].signature != DBusSignature('a{oa{sa{sv}}}')) {
-      throw 'GetManagedObjects returned invalid result: $values';
+    if (result.signature != DBusSignature('a{oa{sa{sv}}}')) {
+      throw 'GetManagedObjects returned invalid result: ${result.returnValues}';
     }
 
     Map<DBusObjectPath, Map<String, Map<String, DBusValue>>> decodeObjects(
@@ -179,7 +170,7 @@ class DBusRemoteObject {
           key as DBusObjectPath, _decodeInterfacesAndProperties(value)));
     }
 
-    return decodeObjects(values[0]);
+    return decodeObjects(result.returnValues[0]);
   }
 
   /// Subscribes to signals using object manager.
@@ -191,22 +182,15 @@ class DBusRemoteObject {
     return signals.map((signal) {
       if (signal.interface == 'org.freedesktop.DBus.ObjectManager' &&
           signal.name == 'InterfacesAdded' &&
-          signal.values.length == 2 &&
-          signal.values[0].signature == DBusSignature('o') &&
-          signal.values[1].signature == DBusSignature('a{sa{sv}}')) {
+          signal.signature == DBusSignature('oa{sa{sv}}')) {
         return DBusObjectManagerInterfacesAddedSignal(signal);
       } else if (signal.interface == 'org.freedesktop.DBus.ObjectManager' &&
           signal.name == 'InterfacesRemoved' &&
-          signal.values.length == 2 &&
-          signal.values[0].signature == DBusSignature('o') &&
-          signal.values[1].signature == DBusSignature('as')) {
+          signal.signature == DBusSignature('oas')) {
         return DBusObjectManagerInterfacesRemovedSignal(signal);
       } else if (signal.interface == 'org.freedesktop.DBus.Properties' &&
           signal.name == 'PropertiesChanged' &&
-          signal.values.length == 3 &&
-          signal.values[0].signature == DBusSignature('s') &&
-          signal.values[1].signature == DBusSignature('a{sv}') &&
-          signal.values[2].signature == DBusSignature('as')) {
+          signal.signature == DBusSignature('aa{sv}as')) {
         return DBusPropertiesChangedSignal(signal);
       } else {
         return signal;

bin/dart-dbus.dart
@@ -403,25 +403,24 @@ String generateHandleMethodCall(DBusIntrospectNode node) {
     var methodBranches = <SwitchBranch>[];
     for (var method in interface.methods) {
       var argValues = <String>[];
-      var argChecks = <String>[];
       var inputArgs = method.args
           .where((arg) => arg.direction == DBusArgumentDirection.in_);
+      String argCheck;
       if (inputArgs.isEmpty) {
-        argChecks.add('methodCall.values.isNotEmpty');
+        argCheck = 'methodCall.values.isNotEmpty';
       } else {
-        argChecks.add('methodCall.values.length != ${argValues.length}');
+        argCheck =
+            "methodCall.signature != DBusSignature('${method.inputSignature.value}')";
       }
       for (var arg in inputArgs) {
         var argName = 'methodCall.values[${argValues.length}]';
-        argChecks
-            .add("$argName.signature != DBusSignature('${arg.type.value}')");
         var type = getDartType(arg.type);
         var convertedValue = type.dbusToNative(argName);
         argValues.add(convertedValue);
       }
 
       var source = '';
-      source += 'if (${argChecks.join(' || ')}) {\n';
+      source += 'if ($argCheck) {\n';
       source += '  return DBusMethodErrorResponse.invalidArgs();\n';
       source += '}\n';
       source += 'return do${method.name}(${argValues.join(', ')});\n';
@@ -798,18 +797,12 @@ String generateRemoteSignalSubscription(
     String classPrefix,
     DBusIntrospectInterface interface,
     DBusIntrospectSignal signal) {
-  var index = 0;
-  var valueChecks = <String>[];
+  String valueCheck;
   if (signal.args.isEmpty) {
-    valueChecks.add('signal.values.isEmpty');
+    valueCheck = 'signal.values.isEmpty';
   } else {
-    valueChecks.add('signal.values.length == ${signal.args.length}');
-  }
-  for (var arg in signal.args) {
-    var valueName = 'signal.values[$index]';
-    valueChecks
-        .add("$valueName.signature == DBusSignature('${arg.type.value}')");
-    index++;
+    valueCheck =
+        "signal.signature == DBusSignature('${signal.signature.value}')";
   }
 
   var methodName = getUniqueMethodName(methodNames, 'subscribe${signal.name}');
@@ -820,7 +813,7 @@ String generateRemoteSignalSubscription(
   source +=
       "    var signals = subscribeSignal('${interface.name}', '${signal.name}');\n";
   source += '    return signals.map((signal) {\n';
-  source += '      if (${valueChecks.join(' && ')}) {\n';
+  source += '      if ($valueCheck) {\n';
   source += '        return $classPrefix${signal.name}(signal);\n';
   source += '      } else {\n';
   source +=

lib/src/dbus_client.dart
@@ -167,9 +167,8 @@ class DBusClient {
         interface: 'org.freedesktop.DBus',
         member: 'RequestName',
         values: [DBusString(name), DBusUint32(flagsValue)]);
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('u')) {
-      throw 'org.freedesktop.DBus.RequestName returned invalid result: $values';
+    if (result.signature != DBusSignature('u')) {
+      throw 'org.freedesktop.DBus.RequestName returned invalid result: ${result.returnValues}';
     }
     var returnCode = (result.returnValues[0] as DBusUint32).value;
     switch (returnCode) {
@@ -195,9 +194,8 @@ class DBusClient {
         interface: 'org.freedesktop.DBus',
         member: 'ReleaseName',
         values: [DBusString(name)]);
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('u')) {
-      throw 'org.freedesktop.DBus.ReleaseName returned invalid result: $values';
+    if (result.signature != DBusSignature('u')) {
+      throw 'org.freedesktop.DBus.ReleaseName returned invalid result: ${result.returnValues}';
     }
     var returnCode = (result.returnValues[0] as DBusUint32).value;
     switch (returnCode) {
@@ -220,11 +218,10 @@ class DBusClient {
         path: DBusObjectPath('/org/freedesktop/DBus'),
         interface: 'org.freedesktop.DBus',
         member: 'ListQueuedOwners');
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('as')) {
-      throw 'org.freedesktop.DBus.ListQueuedOwners returned invalid result: $values';
+    if (result.signature != DBusSignature('as')) {
+      throw 'org.freedesktop.DBus.ListQueuedOwners returned invalid result: ${result.returnValues}';
     }
-    return (values[0] as DBusArray)
+    return (result.returnValues[0] as DBusArray)
         .children
         .map((v) => (v as DBusString).value)
         .toList();
@@ -237,11 +234,10 @@ class DBusClient {
         path: DBusObjectPath('/org/freedesktop/DBus'),
         interface: 'org.freedesktop.DBus',
         member: 'ListNames');
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('as')) {
-      throw 'org.freedesktop.DBus.ListNames returned invalid result: $values';
+    if (result.signature != DBusSignature('as')) {
+      throw 'org.freedesktop.DBus.ListNames returned invalid result: ${result.returnValues}';
     }
-    return (values[0] as DBusArray)
+    return (result.returnValues[0] as DBusArray)
         .children
         .map((v) => (v as DBusString).value)
         .toList();
@@ -254,11 +250,10 @@ class DBusClient {
         path: DBusObjectPath('/org/freedesktop/DBus'),
         interface: 'org.freedesktop.DBus',
         member: 'ListActivatableNames');
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('as')) {
-      throw 'org.freedesktop.DBus.ListActivatableNames returned invalid result: $values';
+    if (result.signature != DBusSignature('as')) {
+      throw 'org.freedesktop.DBus.ListActivatableNames returned invalid result: ${result.returnValues}';
     }
-    return (values[0] as DBusArray)
+    return (result.returnValues[0] as DBusArray)
         .children
         .map((v) => (v as DBusString).value)
         .toList();
@@ -272,11 +267,10 @@ class DBusClient {
         interface: 'org.freedesktop.DBus',
         member: 'NameHasOwner',
         values: [DBusString(name)]);
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('b')) {
-      throw 'org.freedesktop.DBus.NameHasOwner returned invalid result: $values';
+    if (result.signature != DBusSignature('b')) {
+      throw 'org.freedesktop.DBus.NameHasOwner returned invalid result: ${result.returnValues}';
     }
-    return (values[0] as DBusBoolean).value;
+    return (result.returnValues[0] as DBusBoolean).value;
   }
 
   /// Returns the unique connection name of the client that owns [name].
@@ -291,11 +285,10 @@ class DBusClient {
         result.errorName == 'org.freedesktop.DBus.Error.NameHasNoOwner') {
       return null;
     }
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('s')) {
-      throw 'org.freedesktop.DBus.GetNameOwner returned invalid result: $values';
+    if (result.signature != DBusSignature('s')) {
+      throw 'org.freedesktop.DBus.GetNameOwner returned invalid result: ${result.returnValues}';
     }
-    return (values[0] as DBusString).value;
+    return (result.returnValues[0] as DBusString).value;
   }
 
   /// Gets the unique ID of the bus.
@@ -305,11 +298,10 @@ class DBusClient {
         path: DBusObjectPath('/org/freedesktop/DBus'),
         interface: 'org.freedesktop.DBus',
         member: 'GetId');
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('s')) {
-      throw 'org.freedesktop.DBus.GetId returned invalid result: $values';
+    if (result.signature != DBusSignature('s')) {
+      throw 'org.freedesktop.DBus.GetId returned invalid result: ${result.returnValues}';
     }
-    return (values[0] as DBusString).value;
+    return (result.returnValues[0] as DBusString).value;
   }
 
   /// Sends a ping request to the client at the given [destination].
@@ -334,11 +326,10 @@ class DBusClient {
         path: DBusObjectPath('/'),
         interface: 'org.freedesktop.DBus.Peer',
         member: 'GetMachineId');
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('s')) {
-      throw 'org.freedesktop.DBus.Peer.GetMachineId returned invalid result: $values';
+    if (result.signature != DBusSignature('s')) {
+      throw 'org.freedesktop.DBus.Peer.GetMachineId returned invalid result: ${result.returnValues}';
     }
-    return (values[0] as DBusString).value;
+    return (result.returnValues[0] as DBusString).value;
   }
 
   /// Invokes a method on a D-Bus object.
@@ -463,11 +454,10 @@ class DBusClient {
         'Hello',
         [],
         requireConnect: false);
-    var values = result.returnValues;
-    if (values.length != 1 || values[0].signature != DBusSignature('s')) {
-      throw 'org.freedesktop.DBus.Hello returned invalid result: $values';
+    if (result.signature != DBusSignature('s')) {
+      throw 'org.freedesktop.DBus.Hello returned invalid result: ${result.returnValues}';
     }
-    _uniqueName = (values[0] as DBusString).value;
+    _uniqueName = (result.returnValues[0] as DBusString).value;
 
     // Notify anyone else awaiting connection.
     _connectCompleter?.complete();
@@ -493,8 +483,7 @@ class DBusClient {
 
   /// Handles the org.freedesktop.DBus.NameAcquired signal.
   void _handleNameAcquired(DBusSignal signal) {
-    if (signal.values.length != 1 ||
-        signal.values[0].signature != DBusSignature('s')) {
+    if (signal.signature != DBusSignature('s')) {
       throw 'org.freedesktop.DBus.NameAcquired received with invalid arguments: ${signal.values}';
     }
 
@@ -508,8 +497,7 @@ class DBusClient {
 
   /// Handles the org.freedesktop.DBus.NameLost signal.
   void _handleNameLost(DBusSignal signal) {
-    if (signal.values.length != 1 ||
-        signal.values[0].signature != DBusSignature('s')) {
+    if (signal.signature != DBusSignature('s')) {
       throw 'org.freedesktop.DBus.NameLost received with invalid arguments: ${signal.values}';
     }
 
@@ -523,10 +511,7 @@ class DBusClient {
 
   /// Handles the org.freedesktop.DBus.NameOwnerChanged signal and updates the table of known names.
   void _handleNameOwnerChanged(DBusSignal signal) {
-    if (signal.values.length != 3 ||
-        signal.values[0].signature != DBusSignature('s') ||
-        signal.values[1].signature != DBusSignature('s') ||
-        signal.values[2].signature != DBusSignature('s')) {
+    if (signal.signature != DBusSignature('sss')) {
       throw 'org.freedesktop.DBus.NameOwnerChanged received with invalid arguments: ${signal.values}';
     }
 
@@ -549,9 +534,8 @@ class DBusClient {
           interface: 'org.freedesktop.DBus',
           member: 'AddMatch',
           values: [DBusString(rule)]);
-      var values = result.returnValues;
-      if (values.isNotEmpty) {
-        throw 'org.freedesktop.DBus.AddMatch returned invalid result: $values';
+      if (result.returnValues.isNotEmpty) {
+        throw 'org.freedesktop.DBus.AddMatch returned invalid result: ${result.returnValues}';
       }
       count = 1;
     } else {
@@ -574,9 +558,8 @@ class DBusClient {
           interface: 'org.freedesktop.DBus',
           member: 'RemoveMatch',
           values: [DBusString(rule)]);
-      var values = result.returnValues;
-      if (values.isNotEmpty) {
-        throw 'org.freedesktop.DBus.RemoveMatch returned invalid result: $values';
+      if (result.returnValues.isNotEmpty) {
+        throw 'org.freedesktop.DBus.RemoveMatch returned invalid result: ${result.returnValues}';
       }
       _matchRules.remove(rule);
     } else {

lib/src/dbus_introspect.dart
@@ -129,6 +129,18 @@ class DBusIntrospectMethod {
   /// Annotations for this method.
   final List<DBusIntrospectAnnotation> annotations;
 
+  /// The signature for the input arguments in this method call.
+  DBusSignature get inputSignature => args
+      .where((arg) => arg.direction == DBusArgumentDirection.in_)
+      .map((arg) => arg.type)
+      .reduce((a, b) => a + b);
+
+  /// The signature for the output arguments in this method call.
+  DBusSignature get outputSignature => args
+      .where((arg) => arg.direction == DBusArgumentDirection.out)
+      .map((arg) => arg.type)
+      .reduce((a, b) => a + b);
+
   DBusIntrospectMethod(this.name,
       {this.args = const [], this.annotations = const []});
 
@@ -173,6 +185,10 @@ class DBusIntrospectSignal {
 
   /// Annotations for this signal.
   final List<DBusIntrospectAnnotation> annotations;
+
+  /// The signature for the arguments in this signals.
+  DBusSignature get signature =>
+      args.map((arg) => arg.type).reduce((a, b) => a + b);
 
   DBusIntrospectSignal(this.name,
       {this.args = const [], this.annotations = const []});

lib/src/dbus_method_call.dart
@@ -14,6 +14,10 @@ class DBusMethodCall {
   /// Arguments passed by caller.
   final List<DBusValue> values;
 
+  /// Signature of [values].
+  DBusSignature get signature =>
+      values.map((value) => value.signature).reduce((a, b) => a + b);
+
   const DBusMethodCall(this.sender, this.interface, this.name, this.values);
 
   @override

lib/src/dbus_method_response.dart
@@ -4,6 +4,10 @@ import 'dbus_value.dart';
 abstract class DBusMethodResponse {
   /// Gets the value returned from this method or throws an exception if an error received.
   List<DBusValue> get returnValues;
+
+  /// Gets the signature of the [returnValues].
+  DBusSignature get signature =>
+      returnValues.map((value) => value.signature).reduce((a, b) => a + b);
 }
 
 /// A success response to a method call.

lib/src/dbus_signal.dart
@@ -17,6 +17,10 @@ class DBusSignal {
   /// Values associated with the signal.
   final List<DBusValue> values;
 
+  /// Signature of [values].
+  DBusSignature get signature =>
+      values.map((value) => value.signature).reduce((a, b) => a + b);
+
   const DBusSignal(
       this.sender, this.path, this.interface, this.name, this.values);
 

lib/src/dbus_value.dart
@@ -465,6 +465,9 @@ class DBusSignature extends DBusValue {
   @override
   bool operator ==(other) => other is DBusSignature && other.value == value;
 
+  DBusSignature operator +(DBusSignature other) =>
+      DBusSignature(value + other.value);
+
   @override
   int get hashCode => value.hashCode;
 

commit cd749a08d5aa9b5ce91717428c9f2088454b55b0

Fix PropertiesChanged signal not being correctly detected.
Broken in 590fe7086cba1dea8390e4c0800a3aacb0b24df2

lib/src/dbus_remote_object.dart
@@ -127,7 +127,7 @@ class DBusRemoteObject {
     var signals =
         subscribeSignal('org.freedesktop.DBus.Properties', 'PropertiesChanged');
     return signals.map((signal) {
-      if (signal.signature != DBusSignature('sa{sv}as')) {
+      if (signal.signature == DBusSignature('sa{sv}as')) {
         return DBusPropertiesChangedSignal(signal);
       } else {
         throw 'org.freedesktop.DBus.Properties.PropertiesChanged contains invalid values ${signal.values}';