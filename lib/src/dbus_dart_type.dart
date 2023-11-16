canonical/dbus.dart

commit 544b44f6e4dd1a54b6792c935056303dd5937f42

Add helper methods to DBusValue to reduce casting.

lib/src/dbus_dart_type.dart
@@ -51,10 +51,10 @@ abstract class DBusDartType {
   // Native Dart type for the API user to interact with, e.g. 'int', 'String'.
   String get nativeType;
 
-  // Converts a native Dart variable to a D-Bus data type. e.g. 'foo' -> 'DBusInteger(foo)'.
+  // Converts a native Dart variable to a D-Bus data type. e.g. 'foo' -> 'DBusInt32(foo)'.
   String nativeToDBus(String name);
 
-  // Converts a DBusValue object to a native type. e.g. 'foo' -> '(foo as DBusInteger).value'.
+  // Converts a DBusValue object to a native type. e.g. 'foo' -> 'foo.asInt32()'.
   String dbusToNative(String name);
 }
 
@@ -72,7 +72,7 @@ class DBusBoolType extends DBusDartType {
 
   @override
   String dbusToNative(String name) {
-    return '($name as DBusBoolean).value';
+    return '$name.asBoolean()';
   }
 }
 
@@ -92,7 +92,7 @@ class DBusByteType extends DBusIntegerType {
 
   @override
   String dbusToNative(String name) {
-    return '($name as DBusByte).value';
+    return '$name.asByte()';
   }
 }
 
@@ -105,7 +105,7 @@ class DBusInt16Type extends DBusIntegerType {
 
   @override
   String dbusToNative(String name) {
-    return '($name as DBusInt16).value';
+    return '$name.asInt16()';
   }
 }
 
@@ -118,7 +118,7 @@ class DBusUint16Type extends DBusIntegerType {
 
   @override
   String dbusToNative(String name) {
-    return '($name as DBusUint16).value';
+    return '$name.asUint16()';
   }
 }
 
@@ -131,7 +131,7 @@ class DBusInt32Type extends DBusIntegerType {
 
   @override
   String dbusToNative(String name) {
-    return '($name as DBusInt32).value';
+    return '$name.asInt32()';
   }
 }
 
@@ -144,7 +144,7 @@ class DBusUint32Type extends DBusIntegerType {
 
   @override
   String dbusToNative(String name) {
-    return '($name as DBusUint32).value';
+    return '$name.asUint32()';
   }
 }
 
@@ -157,7 +157,7 @@ class DBusInt64Type extends DBusIntegerType {
 
   @override
   String dbusToNative(String name) {
-    return '($name as DBusInt64).value';
+    return '$name.asInt64()';
   }
 }
 
@@ -170,7 +170,7 @@ class DBusUint64Type extends DBusIntegerType {
 
   @override
   String dbusToNative(String name) {
-    return '($name as DBusUint64).value';
+    return '$name.asUint64()';
   }
 }
 
@@ -188,7 +188,7 @@ class DBusDoubleType extends DBusDartType {
 
   @override
   String dbusToNative(String name) {
-    return '($name as DBusDouble).value';
+    return '$name.asDouble()';
   }
 }
 
@@ -206,7 +206,7 @@ class DBusStringType extends DBusDartType {
 
   @override
   String dbusToNative(String name) {
-    return '($name as DBusString).value';
+    return '$name.asString()';
   }
 }
 
@@ -219,7 +219,7 @@ class DBusObjectPathType extends DBusStringType {
 
   @override
   String dbusToNative(String name) {
-    return '($name as DBusObjectPath).value';
+    return '$name.asObjectPath()';
   }
 }
 
@@ -237,7 +237,7 @@ class DBusSignatureType extends DBusDartType {
 
   @override
   String dbusToNative(String name) {
-    return '$name as DBusSignature';
+    return '$name.asSignature()';
   }
 }
 
@@ -255,7 +255,7 @@ class DBusVariantType extends DBusDartType {
 
   @override
   String dbusToNative(String name) {
-    return '($name as DBusVariant).value';
+    return '$name.asVariant()';
   }
 }
 
@@ -273,7 +273,7 @@ class DBusStructType extends DBusDartType {
 
   @override
   String dbusToNative(String name) {
-    return '$name as DBusStruct';
+    return '$name.asStruct()';
   }
 }
 
@@ -327,33 +327,33 @@ class DBusArrayType extends DBusDartType {
   String dbusToNative(String name) {
     switch (childSignature.value) {
       case 'y':
-        return '($name as DBusArray).mapByte().toList()';
+        return '$name.asByteArray().toList()';
       case 'n':
-        return '($name as DBusArray).mapInt16().toList()';
+        return '$name.asInt16Array().toList()';
       case 'q':
-        return '($name as DBusArray).mapUint16().toList()';
+        return '$name.asUint16Array().toList()';
       case 'i':
-        return '($name as DBusArray).mapInt32().toList()';
+        return '$name.asInt32Array().toList()';
       case 'u':
-        return '($name as DBusArray).mapUint32().toList()';
+        return '$name.asUint32Array().toList()';
       case 'x':
-        return '($name as DBusArray).mapInt64().toList()';
+        return '$name.asInt64Array().toList()';
       case 't':
-        return '($name as DBusArray).mapUint64().toList()';
+        return '$name.asUint64Array().toList()';
       case 'd':
-        return '($name as DBusArray).mapDouble().toList()';
+        return '$name.asDoubleArray().toList()';
       case 's':
-        return '($name as DBusArray).mapString().toList()';
+        return '$name.asStringArray().toList()';
       case 'o':
-        return '($name as DBusArray).objectPath().toList()';
+        return '$name.asObjectPathArray().toList()';
       case 'g':
-        return '($name as DBusArray).signature().toList()';
+        return '$name.asSignatureArray().toList()';
       case 'v':
-        return '($name as DBusArray).mapVariant().toList()';
+        return '$name.asVariantArray().toList()';
       default:
         var childType = getDartType(childSignature);
         var convertedValue = childType.dbusToNative('child');
-        return '($name as DBusArray).children.map((child) => $convertedValue).toList()';
+        return '$name.asArray().map((child) => $convertedValue).toList()';
     }
   }
 }
@@ -390,13 +390,13 @@ class DBusDictType extends DBusDartType {
   String dbusToNative(String name) {
     if (keySignature == DBusSignature('s') &&
         valueSignature == DBusSignature('v')) {
-      return '($name as DBusDict).mapStringVariant()';
+      return '$name.asStringVariantDict()';
     } else {
       var keyType = getDartType(keySignature);
       var convertedKey = keyType.dbusToNative('key');
       var valueType = getDartType(valueSignature);
       var convertedValue = valueType.dbusToNative('value');
-      return '($name as DBusDict).children.map((key, value) => MapEntry($convertedKey, $convertedValue))';
+      return '$name.asDict().map((key, value) => MapEntry($convertedKey, $convertedValue))';
     }
   }
 }

test/generated-code/method-multiple-inputs.server.out
@@ -21,7 +21,7 @@ class ComExampleTest extends DBusObject {
         if (methodCall.signature != DBusSignature('ybnqiuxtdsogv(si)ayabanaqaiauaxatadasaoagava(si)aasa{sv}a{us}a{sa{sv}}')) {
           return DBusMethodErrorResponse.invalidArgs();
         }
-        return doHello((methodCall.values[0] as DBusByte).value, (methodCall.values[1] as DBusBoolean).value, (methodCall.values[2] as DBusInt16).value, (methodCall.values[3] as DBusUint16).value, (methodCall.values[4] as DBusInt32).value, (methodCall.values[5] as DBusUint32).value, (methodCall.values[6] as DBusInt64).value, (methodCall.values[7] as DBusUint64).value, (methodCall.values[8] as DBusDouble).value, (methodCall.values[9] as DBusString).value, (methodCall.values[10] as DBusObjectPath).value, methodCall.values[11] as DBusSignature, (methodCall.values[12] as DBusVariant).value, methodCall.values[13] as DBusStruct, (methodCall.values[14] as DBusArray).mapByte().toList(), (methodCall.values[15] as DBusArray).children.map((child) => (child as DBusBoolean).value).toList(), (methodCall.values[16] as DBusArray).mapInt16().toList(), (methodCall.values[17] as DBusArray).mapUint16().toList(), (methodCall.values[18] as DBusArray).mapInt32().toList(), (methodCall.values[19] as DBusArray).mapUint32().toList(), (methodCall.values[20] as DBusArray).mapInt64().toList(), (methodCall.values[21] as DBusArray).mapUint64().toList(), (methodCall.values[22] as DBusArray).mapDouble().toList(), (methodCall.values[23] as DBusArray).mapString().toList(), (methodCall.values[24] as DBusArray).objectPath().toList(), (methodCall.values[25] as DBusArray).signature().toList(), (methodCall.values[26] as DBusArray).mapVariant().toList(), (methodCall.values[27] as DBusArray).children.map((child) => child as DBusStruct).toList(), (methodCall.values[28] as DBusArray).children.map((child) => (child as DBusArray).mapString().toList()).toList(), (methodCall.values[29] as DBusDict).mapStringVariant(), (methodCall.values[30] as DBusDict).children.map((key, value) => MapEntry((key as DBusUint32).value, (value as DBusString).value)), (methodCall.values[31] as DBusDict).children.map((key, value) => MapEntry((key as DBusString).value, (value as DBusDict).mapStringVariant())));
+        return doHello(methodCall.values[0].asByte(), methodCall.values[1].asBoolean(), methodCall.values[2].asInt16(), methodCall.values[3].asUint16(), methodCall.values[4].asInt32(), methodCall.values[5].asUint32(), methodCall.values[6].asInt64(), methodCall.values[7].asUint64(), methodCall.values[8].asDouble(), methodCall.values[9].asString(), methodCall.values[10].asObjectPath(), methodCall.values[11].asSignature(), methodCall.values[12].asVariant(), methodCall.values[13].asStruct(), methodCall.values[14].asByteArray().toList(), methodCall.values[15].asArray().map((child) => child.asBoolean()).toList(), methodCall.values[16].asInt16Array().toList(), methodCall.values[17].asUint16Array().toList(), methodCall.values[18].asInt32Array().toList(), methodCall.values[19].asUint32Array().toList(), methodCall.values[20].asInt64Array().toList(), methodCall.values[21].asUint64Array().toList(), methodCall.values[22].asDoubleArray().toList(), methodCall.values[23].asStringArray().toList(), methodCall.values[24].asObjectPathArray().toList(), methodCall.values[25].asSignatureArray().toList(), methodCall.values[26].asVariantArray().toList(), methodCall.values[27].asArray().map((child) => child.asStruct()).toList(), methodCall.values[28].asArray().map((child) => child.asStringArray().toList()).toList(), methodCall.values[29].asStringVariantDict(), methodCall.values[30].asDict().map((key, value) => MapEntry(key.asUint32(), value.asString())), methodCall.values[31].asDict().map((key, value) => MapEntry(key.asString(), value.asStringVariantDict())));
       } else {
         return DBusMethodErrorResponse.unknownMethod();
       }

test/generated-code/properties.client.out
@@ -6,7 +6,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.ByteProperty
   Future<int> getByteProperty() async {
     var value = await getProperty('com.example.Test', 'ByteProperty', signature: DBusSignature('y'));
-    return (value as DBusByte).value;
+    return value.asByte();
   }
 
   /// Sets com.example.Test.ByteProperty
@@ -17,7 +17,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.BooleanProperty
   Future<bool> getBooleanProperty() async {
     var value = await getProperty('com.example.Test', 'BooleanProperty', signature: DBusSignature('b'));
-    return (value as DBusBoolean).value;
+    return value.asBoolean();
   }
 
   /// Sets com.example.Test.BooleanProperty
@@ -28,7 +28,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.Int16Property
   Future<int> getInt16Property() async {
     var value = await getProperty('com.example.Test', 'Int16Property', signature: DBusSignature('n'));
-    return (value as DBusInt16).value;
+    return value.asInt16();
   }
 
   /// Sets com.example.Test.Int16Property
@@ -39,7 +39,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.Uint16Property
   Future<int> getUint16Property() async {
     var value = await getProperty('com.example.Test', 'Uint16Property', signature: DBusSignature('q'));
-    return (value as DBusUint16).value;
+    return value.asUint16();
   }
 
   /// Sets com.example.Test.Uint16Property
@@ -50,7 +50,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.Int32Property
   Future<int> getInt32Property() async {
     var value = await getProperty('com.example.Test', 'Int32Property', signature: DBusSignature('i'));
-    return (value as DBusInt32).value;
+    return value.asInt32();
   }
 
   /// Sets com.example.Test.Int32Property
@@ -61,7 +61,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.Uint32Property
   Future<int> getUint32Property() async {
     var value = await getProperty('com.example.Test', 'Uint32Property', signature: DBusSignature('u'));
-    return (value as DBusUint32).value;
+    return value.asUint32();
   }
 
   /// Sets com.example.Test.Uint32Property
@@ -72,7 +72,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.Int64Property
   Future<int> getInt64Property() async {
     var value = await getProperty('com.example.Test', 'Int64Property', signature: DBusSignature('x'));
-    return (value as DBusInt64).value;
+    return value.asInt64();
   }
 
   /// Sets com.example.Test.Int64Property
@@ -83,7 +83,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.Uint64Property
   Future<int> getUint64Property() async {
     var value = await getProperty('com.example.Test', 'Uint64Property', signature: DBusSignature('t'));
-    return (value as DBusUint64).value;
+    return value.asUint64();
   }
 
   /// Sets com.example.Test.Uint64Property
@@ -94,7 +94,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.DoubleProperty
   Future<double> getDoubleProperty() async {
     var value = await getProperty('com.example.Test', 'DoubleProperty', signature: DBusSignature('d'));
-    return (value as DBusDouble).value;
+    return value.asDouble();
   }
 
   /// Sets com.example.Test.DoubleProperty
@@ -105,7 +105,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.StringProperty
   Future<String> getStringProperty() async {
     var value = await getProperty('com.example.Test', 'StringProperty', signature: DBusSignature('s'));
-    return (value as DBusString).value;
+    return value.asString();
   }
 
   /// Sets com.example.Test.StringProperty
@@ -116,7 +116,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.ObjectPathProperty
   Future<String> getObjectPathProperty() async {
     var value = await getProperty('com.example.Test', 'ObjectPathProperty', signature: DBusSignature('o'));
-    return (value as DBusObjectPath).value;
+    return value.asObjectPath();
   }
 
   /// Sets com.example.Test.ObjectPathProperty
@@ -127,7 +127,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.SignatureProperty
   Future<DBusSignature> getSignatureProperty() async {
     var value = await getProperty('com.example.Test', 'SignatureProperty', signature: DBusSignature('g'));
-    return value as DBusSignature;
+    return value.asSignature();
   }
 
   /// Sets com.example.Test.SignatureProperty
@@ -138,7 +138,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.VariantProperty
   Future<DBusValue> getVariantProperty() async {
     var value = await getProperty('com.example.Test', 'VariantProperty', signature: DBusSignature('v'));
-    return (value as DBusVariant).value;
+    return value.asVariant();
   }
 
   /// Sets com.example.Test.VariantProperty
@@ -149,7 +149,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.StructProperty
   Future<DBusStruct> getStructProperty() async {
     var value = await getProperty('com.example.Test', 'StructProperty', signature: DBusSignature('(si)'));
-    return value as DBusStruct;
+    return value.asStruct();
   }
 
   /// Sets com.example.Test.StructProperty
@@ -160,7 +160,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.ArrayProperty
   Future<List<int>> getArrayProperty() async {
     var value = await getProperty('com.example.Test', 'ArrayProperty', signature: DBusSignature('ay'));
-    return (value as DBusArray).mapByte().toList();
+    return value.asByteArray().toList();
   }
 
   /// Sets com.example.Test.ArrayProperty
@@ -171,7 +171,7 @@ class ComExampleTest extends DBusRemoteObject {
   /// Gets com.example.Test.DictProperty
   Future<Map<String, DBusValue>> getDictProperty() async {
     var value = await getProperty('com.example.Test', 'DictProperty', signature: DBusSignature('a{sv}'));
-    return (value as DBusDict).mapStringVariant();
+    return value.asStringVariantDict();
   }
 
   /// Sets com.example.Test.DictProperty

commit 94d967848ace397f823ebe8c4459e6c7f04f5aec

Fix generated code that uses DBusStruct.
This was broken in 544b44f6e4dd1a54b6792c935056303dd5937f42 when the data type was changed from DBusStruct to List<DBusValue>

lib/src/dbus_dart_type.dart
@@ -283,7 +283,7 @@ class DBusUnixFdType extends DBusDartType {
 class DBusStructType extends DBusDartType {
   @override
   String get nativeType {
-    return 'DBusStruct';
+    return 'List<DBusValue>';
   }
 
   @override

test/generated-code/method-multiple-inputs.client.out
@@ -4,7 +4,7 @@ class ComExampleTest extends DBusRemoteObject {
   ComExampleTest(DBusClient client, String destination, DBusObjectPath path) : super(client, name: destination, path: path);
 
   /// Invokes com.example.Test.Hello()
-  Future<void> callHello(int byte_value, bool boolean_value, int int16_value, int uint16_value, int int32_value, int uint32_value, int int64_value, int uint64_value, double double_value, String string_value, String object_path_value, DBusSignature signature_value, DBusValue variant_value, ResourceHandle unix_fd_value, DBusStruct struct_value, List<int> array_value_y, List<bool> array_value_b, List<int> array_value_n, List<int> array_value_q, List<int> array_value_u, List<int> array_value_n, List<int> array_value_x, List<int> array_value_t, List<double> array_value_d, List<String> array_value_s, List<String> array_value_o, List<DBusSignature> array_value_g, List<DBusValue> array_value_v, List<ResourceHandle> array_value_h, List<DBusStruct> array_value_struct, List<List<String>> array_value_array, Map<String, DBusValue> dict_value_sv, Map<int, String> dict_value_us, Map<String, Map<String, DBusValue>> dict_value_dict, {bool noAutoStart = false, bool allowInteractiveAuthorization = false}) async {
+  Future<void> callHello(int byte_value, bool boolean_value, int int16_value, int uint16_value, int int32_value, int uint32_value, int int64_value, int uint64_value, double double_value, String string_value, String object_path_value, DBusSignature signature_value, DBusValue variant_value, ResourceHandle unix_fd_value, List<DBusValue> struct_value, List<int> array_value_y, List<bool> array_value_b, List<int> array_value_n, List<int> array_value_q, List<int> array_value_u, List<int> array_value_n, List<int> array_value_x, List<int> array_value_t, List<double> array_value_d, List<String> array_value_s, List<String> array_value_o, List<DBusSignature> array_value_g, List<DBusValue> array_value_v, List<ResourceHandle> array_value_h, List<List<DBusValue>> array_value_struct, List<List<String>> array_value_array, Map<String, DBusValue> dict_value_sv, Map<int, String> dict_value_us, Map<String, Map<String, DBusValue>> dict_value_dict, {bool noAutoStart = false, bool allowInteractiveAuthorization = false}) async {
     await callMethod('com.example.Test', 'Hello', [DBusByte(byte_value), DBusBoolean(boolean_value), DBusInt16(int16_value), DBusUint16(uint16_value), DBusInt32(int32_value), DBusUint32(uint32_value), DBusInt64(int64_value), DBusUint64(uint64_value), DBusDouble(double_value), DBusString(string_value), DBusObjectPath(object_path_value), signature_value, DBusVariant(variant_value), DBusUnixFd(unix_fd_value), struct_value, DBusArray.byte(array_value_y), DBusArray.boolean(array_value_b), DBusArray.int16(array_value_n), DBusArray.uint16(array_value_q), DBusArray.int32(array_value_u), DBusArray.uint32(array_value_n), DBusArray.int64(array_value_x), DBusArray.uint64(array_value_t), DBusArray.double(array_value_d), DBusArray.string(array_value_s), DBusArray.objectPath(array_value_o), DBusArray.signature(array_value_g), DBusArray.variant(array_value_v), DBusArray.unixFd(array_value_h), DBusArray(DBusSignature('(si)'), array_value_struct.map((child) => child)), DBusArray(DBusSignature('as'), array_value_array.map((child) => DBusArray.string(child))), DBusDict.stringVariant(dict_value_sv), DBusDict(DBusSignature('u'), DBusSignature('s'), dict_value_us.map((key, value) => MapEntry(DBusUint32(key), DBusString(value)))), DBusDict(DBusSignature('s'), DBusSignature('a{sv}'), dict_value_dict.map((key, value) => MapEntry(DBusString(key), DBusDict.stringVariant(value))))], replySignature: DBusSignature(''), noAutoStart: noAutoStart, allowInteractiveAuthorization: allowInteractiveAuthorization);
   }
 }

test/generated-code/method-multiple-inputs.server.out
@@ -5,7 +5,7 @@ class ComExampleTest extends DBusObject {
   ComExampleTest({DBusObjectPath path = const DBusObjectPath.unchecked('/')}) : super(path);
 
   /// Implementation of com.example.Test.Hello()
-  Future<DBusMethodResponse> doHello(int byte_value, bool boolean_value, int int16_value, int uint16_value, int int32_value, int uint32_value, int int64_value, int uint64_value, double double_value, String string_value, String object_path_value, DBusSignature signature_value, DBusValue variant_value, ResourceHandle unix_fd_value, DBusStruct struct_value, List<int> array_value_y, List<bool> array_value_b, List<int> array_value_n, List<int> array_value_q, List<int> array_value_u, List<int> array_value_n, List<int> array_value_x, List<int> array_value_t, List<double> array_value_d, List<String> array_value_s, List<String> array_value_o, List<DBusSignature> array_value_g, List<DBusValue> array_value_v, List<ResourceHandle> array_value_h, List<DBusStruct> array_value_struct, List<List<String>> array_value_array, Map<String, DBusValue> dict_value_sv, Map<int, String> dict_value_us, Map<String, Map<String, DBusValue>> dict_value_dict) async {
+  Future<DBusMethodResponse> doHello(int byte_value, bool boolean_value, int int16_value, int uint16_value, int int32_value, int uint32_value, int int64_value, int uint64_value, double double_value, String string_value, String object_path_value, DBusSignature signature_value, DBusValue variant_value, ResourceHandle unix_fd_value, List<DBusValue> struct_value, List<int> array_value_y, List<bool> array_value_b, List<int> array_value_n, List<int> array_value_q, List<int> array_value_u, List<int> array_value_n, List<int> array_value_x, List<int> array_value_t, List<double> array_value_d, List<String> array_value_s, List<String> array_value_o, List<DBusSignature> array_value_g, List<DBusValue> array_value_v, List<ResourceHandle> array_value_h, List<List<DBusValue>> array_value_struct, List<List<String>> array_value_array, Map<String, DBusValue> dict_value_sv, Map<int, String> dict_value_us, Map<String, Map<String, DBusValue>> dict_value_dict) async {
     return DBusMethodErrorResponse.failed('com.example.Test.Hello() not implemented');
   }
 

test/generated-code/properties.client.out
@@ -158,13 +158,13 @@ class ComExampleTest extends DBusRemoteObject {
   }
 
   /// Gets com.example.Test.StructProperty
-  Future<DBusStruct> getStructProperty() async {
+  Future<List<DBusValue>> getStructProperty() async {
     var value = await getProperty('com.example.Test', 'StructProperty', signature: DBusSignature('(si)'));
     return value.asStruct();
   }
 
   /// Sets com.example.Test.StructProperty
-  Future<void> setStructProperty (DBusStruct value) async {
+  Future<void> setStructProperty (List<DBusValue> value) async {
     await setProperty('com.example.Test', 'StructProperty', value);
   }
 

test/generated-code/properties.server.out
@@ -150,7 +150,7 @@ class ComExampleTest extends DBusObject {
   }
 
   /// Sets property com.example.Test.StructProperty
-  Future<DBusMethodResponse> setStructProperty(DBusStruct value) async {
+  Future<DBusMethodResponse> setStructProperty(List<DBusValue> value) async {
     return DBusMethodErrorResponse.failed('Set com.example.Test.StructProperty not implemented');
   }
 

test/generated-code/signal-multiple-args.client.out
@@ -16,7 +16,7 @@ class ComExampleTestEvent extends DBusSignal {
   DBusSignature get signature_value => values[11].asSignature();
   DBusValue get variant_value => values[12].asVariant();
   ResourceHandle get unix_fd_value => values[13].asUnixFd();
-  DBusStruct get struct_value => values[14].asStruct();
+  List<DBusValue> get struct_value => values[14].asStruct();
   List<int> get array_value_y => values[15].asByteArray().toList();
   List<bool> get array_value_b => values[16].asBooleanArray().toList();
   List<int> get array_value_n => values[17].asInt16Array().toList();
@@ -31,7 +31,7 @@ class ComExampleTestEvent extends DBusSignal {
   List<DBusSignature> get array_value_g => values[26].asSignatureArray().toList();
   List<DBusValue> get array_value_v => values[27].asVariantArray().toList();
   List<ResourceHandle> get array_value_h => values[28].asUnixFdArray().toList();
-  List<DBusStruct> get array_value_struct => values[29].asArray().map((child) => child.asStruct()).toList();
+  List<List<DBusValue>> get array_value_struct => values[29].asArray().map((child) => child.asStruct()).toList();
   List<List<String>> get array_value_array => values[30].asArray().map((child) => child.asStringArray().toList()).toList();
   Map<String, DBusValue> get dict_value_sv => values[31].asStringVariantDict();
   Map<int, String> get dict_value_us => values[32].asDict().map((key, value) => MapEntry(key.asUint32(), value.asString()));

test/generated-code/signal-multiple-args.server.out
@@ -5,7 +5,7 @@ class ComExampleTest extends DBusObject {
   ComExampleTest({DBusObjectPath path = const DBusObjectPath.unchecked('/')}) : super(path);
 
   /// Emits signal com.example.Test.Event
-  Future<void> emitEvent(int byte_value, bool boolean_value, int int16_value, int uint16_value, int int32_value, int uint32_value, int int64_value, int uint64_value, double double_value, String string_value, String object_path_value, DBusSignature signature_value, DBusValue variant_value, ResourceHandle unix_fd_value, DBusStruct struct_value, List<int> array_value_y, List<bool> array_value_b, List<int> array_value_n, List<int> array_value_q, List<int> array_value_u, List<int> array_value_n_, List<int> array_value_x, List<int> array_value_t, List<double> array_value_d, List<String> array_value_s, List<String> array_value_o, List<DBusSignature> array_value_g, List<DBusValue> array_value_v, List<ResourceHandle> array_value_h, List<DBusStruct> array_value_struct, List<List<String>> array_value_array, Map<String, DBusValue> dict_value_sv, Map<int, String> dict_value_us, Map<String, Map<String, DBusValue>> dict_value_dict) async {
+  Future<void> emitEvent(int byte_value, bool boolean_value, int int16_value, int uint16_value, int int32_value, int uint32_value, int int64_value, int uint64_value, double double_value, String string_value, String object_path_value, DBusSignature signature_value, DBusValue variant_value, ResourceHandle unix_fd_value, List<DBusValue> struct_value, List<int> array_value_y, List<bool> array_value_b, List<int> array_value_n, List<int> array_value_q, List<int> array_value_u, List<int> array_value_n_, List<int> array_value_x, List<int> array_value_t, List<double> array_value_d, List<String> array_value_s, List<String> array_value_o, List<DBusSignature> array_value_g, List<DBusValue> array_value_v, List<ResourceHandle> array_value_h, List<List<DBusValue>> array_value_struct, List<List<String>> array_value_array, Map<String, DBusValue> dict_value_sv, Map<int, String> dict_value_us, Map<String, Map<String, DBusValue>> dict_value_dict) async {
      await emitSignal('com.example.Test', 'Event', [DBusByte(byte_value), DBusBoolean(boolean_value), DBusInt16(int16_value), DBusUint16(uint16_value), DBusInt32(int32_value), DBusUint32(uint32_value), DBusInt64(int64_value), DBusUint64(uint64_value), DBusDouble(double_value), DBusString(string_value), DBusObjectPath(object_path_value), signature_value, DBusVariant(variant_value), DBusUnixFd(unix_fd_value), struct_value, DBusArray.byte(array_value_y), DBusArray.boolean(array_value_b), DBusArray.int16(array_value_n), DBusArray.uint16(array_value_q), DBusArray.int32(array_value_u), DBusArray.uint32(array_value_n_), DBusArray.int64(array_value_x), DBusArray.uint64(array_value_t), DBusArray.double(array_value_d), DBusArray.string(array_value_s), DBusArray.objectPath(array_value_o), DBusArray.signature(array_value_g), DBusArray.variant(array_value_v), DBusArray.unixFd(array_value_h), DBusArray(DBusSignature('(si)'), array_value_struct.map((child) => child)), DBusArray(DBusSignature('as'), array_value_array.map((child) => DBusArray.string(child))), DBusDict.stringVariant(dict_value_sv), DBusDict(DBusSignature('u'), DBusSignature('s'), dict_value_us.map((key, value) => MapEntry(DBusUint32(key), DBusString(value)))), DBusDict(DBusSignature('s'), DBusSignature('a{sv}'), dict_value_dict.map((key, value) => MapEntry(DBusString(key), DBusDict.stringVariant(value))))]);
   }
 